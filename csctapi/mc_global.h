95abecf08c9248d901a19ba57620c30319d124c4
smurzch2 <smurzch2@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2009-04-04 22:53:51 +0000

    Create basic structure
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@3 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- csctapi/mc_global.h
+++
@@ -1,7 +0,0 @@
-  #define DEV_MULTICAM	"/dev/multicam"
-  #define GET_PCDAT	10
-  #define GET_PADAT	11
-  #define GET_PCDIR	12
-  #define SET_PCDAT	13
-  #define SET_PADAT	14
-  #define SET_PCDIR	15
