380fdb34c014916bc612915ac36a42fdc8112e57
merek <merek@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-02-10 12:31:53 +0000

    Added new counters to oscam userlog
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@1488 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam-log.c
+++ oscam-log.c
@@ -374,13 +374,13 @@ void cs_statistics(int idx)
     else
       cwps=0;
 
-    fprintf(fps, "%02d.%02d.%02d %02d:%02d:%02d %3.1f %s %s %d %d %d %d %ld %ld %s\n", 
+    fprintf(fps, "%02d.%02d.%02d %02d:%02d:%02d %3.1f %s %s %d %d %d %d %d %d %d %ld %ld %s\n", 
                   lt->tm_mday, lt->tm_mon+1, lt->tm_year%100,
                   lt->tm_hour, lt->tm_min, lt->tm_sec, cwps,
                   client[idx].usr[0] ? client[idx].usr : "-",
                   cs_inet_ntoa(client[idx].ip), client[idx].port,
-                  client[idx].cwfound, client[idx].cwcache, client[idx].cwnot,
-                  client[idx].login, client[idx].last,
+                  client[idx].cwfound, client[idx].cwcache, client[idx].cwnot, client[idx].cwignored,
+                  client[idx].cwtout, client[idx].cwtun, client[idx].login, client[idx].last,
                   ph[client[idx].ctyp].desc);
     fflush(fps);
   }
