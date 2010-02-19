3ec63b12b02a3b9e40f62d791a1f89b80eb1394c
landlord <landlord@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-02-19 18:53:18 +0000

    Speed up ATR fetching for SH4 by removing unneccesary code.
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@1620 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- csctapi/ifd_sci.c
+++ csctapi/ifd_sci.c
@@ -39,68 +39,17 @@ int Sci_Reset (ATR * atr)
 	unsigned char buf[SCI_MAX_ATR_SIZE];
 	int n = 0;
 	SCI_PARAMETERS params;
-#ifdef SH4
-	struct timeval tv, tv_spent;
-	int atr_size = 2, TDi_exists = 0;
-#endif
 	
 	memset(&params,0,sizeof(SCI_PARAMETERS));
 	
 	params.ETU = 372; 
 	params.EGT = 3; //not sure why this value is chosen
-#ifdef SH4		
-	params.fs = 9; //not sure why this value is chosen
-#else
 	params.fs = 5;
-#endif
 	params.T = 0;
 	
 	call (ioctl(reader[ridx].handle, IOCTL_SET_PARAMETERS, &params)!=0);
 	call (ioctl(reader[ridx].handle, IOCTL_SET_RESET)<0);
 
-#ifdef SH4
-	gettimeofday(&tv,0);
-	memcpy(&tv_spent,&tv,sizeof(struct timeval));
-
-	while(n<atr_size && (tv_spent.tv_sec-tv.tv_sec)<10)
- 		{
-		if(!IO_Serial_Read(ATR_TIMEOUT, 1, buf+n))
-			n++;
-		gettimeofday(&tv_spent,0);
-		if(n==2) // format character
-		{
-			// high nibble = TA1 , TB1 , TC1 , TD1
-			if(buf[n-1] & 0x10)
-				atr_size++;
-			if(buf[n-1] & 0x20)
-				atr_size++;
-			if(buf[n-1] & 0x40)
-				atr_size++;
-			if(buf[n-1] & 0x80)
-			{
-				atr_size++;
-				TDi_exists=atr_size;
-			}
-			atr_size+=(buf[n-1] & 0x0F); // historical bytes
-		}
-		if( (TDi_exists>0) && (n==TDi_exists) )
-		{
-			TDi_exists=0;
-			// high nibble = TA1 , TB1 , TC1 , TD1
-			if(buf[n-1] & 0x10)
-				atr_size++;
-			if(buf[n-1] & 0x20)
-				atr_size++;
-			if(buf[n-1] & 0x40)
-				atr_size++;
-			if(buf[n-1] & 0x80)
-			{
-				atr_size++;
-				TDi_exists=atr_size;
-			}
-		}
-	}			
-#else
 	while(n<SCI_MAX_ATR_SIZE && !IO_Serial_Read(ATR_TIMEOUT, 1, buf+n))
 	{
 		n++;
@@ -108,7 +57,6 @@ int Sci_Reset (ATR * atr)
 
 	if ((buf[0] !=0x3B) && (buf[0] != 0x3F) && (n>9 && !memcmp(buf+4, "IRDETO", 6))) //irdeto S02 reports FD as first byte on dreambox SCI, not sure about SH4 or phoenix
 		buf[0] = 0x3B;
-#endif
 	
 	if(n==0) {
 		cs_log("ERROR: 0 characters found in ATR");
