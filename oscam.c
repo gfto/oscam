a7ff3d1c5fcff32b7977d83c5a340cb9c1b700ff
landlord <landlord@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-02-26 12:59:52 +0000

    Update credits ;-)
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@1712 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam.c
+++ oscam.c
@@ -127,7 +127,7 @@ static void usage()
   fprintf(stderr, "%s\n\n", logo);
   fprintf(stderr, "OSCam cardserver v%s, build #%s (%s) - (w) 2009-2010 streamboard SVN\n", CS_VERSION_X, CS_SVN_VERSION, CS_OSTYPE);
   fprintf(stderr, "\tsee http://streamboard.gmc.to:8001/wiki/ for more details\n");
-  fprintf(stderr, "\tbased on OSCam 0.99.x, (w) 2010 oscam.ump2002.net SVN\n");
+  fprintf(stderr, "\tbased on OSCam 0.99.x, (w) 2010 streamboard SVN\n");
   fprintf(stderr, "\tbased on streamboard mp-cardserver v0.9d - (w) 2004-2007 by dukat\n");
   fprintf(stderr, "\tinbuilt modules: ");
 #ifdef HAVE_DVBAPI
