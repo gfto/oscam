76b608b1c3e0836d85942e35c2450ee749477ffa
merek <merek@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-03-06 15:07:31 +0000

    More cryptoworks emm fixes
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@1798 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- reader-cryptoworks.c
+++ reader-cryptoworks.c
@@ -465,20 +465,23 @@ int cryptoworks_get_emm_type(EMM_PACKET *ep, struct s_reader * rdr) //returns TR
 			else
 				ep->type = UNKNOWN;
 			break;
+
 		case 0x84:
   	 	if(ep->emm[3]==0xA9 && ep->emm[4]==0xFF && ep->emm[12]==0x80 && ep->emm[13]==0x04)
 				ep->type = SHARED;
 			else
 				ep->type = UNKNOWN;
 			break;
+
+		case 0x88:
 		case 0x89:
   	 	if(ep->emm[3]==0xA9 && ep->emm[4]==0xFF && ep->emm[8]==0x83 && ep->emm[9]==0x01)
 				ep->type = GLOBAL;
 			else
 				ep->type = UNKNOWN;
 			break;
-		case 0x8F://incoming via camd3.5x
-		    //ep->type=emm[4];
+
+		case 0x8F://FIXME incoming emm via camd3.5x, SA/GA/UA ?
 		default:
 			ep->type = UNKNOWN;
 	}
@@ -505,11 +508,12 @@ int cryptoworks_do_emm(EMM_PACKET *ep)
   */
    
   //cs_log("[cryptoworks-reader] EMM Dump:..: %s",cs_hexdump(1, emm, emm[2])); 
-  switch(ep->emm[0])
+  switch(ep->type)
   {
-  	 // emm via camd3.5x
-  	 case 0x8F:  	 	  
-		  if(emm[3]==0xA4)
+  	 case UNKNOWN:
+		  // FIXME emm via camd3.5x was returned from check_emm_type as UNKNOWN
+		  // so we should check here for this emmtype until we know the real mode
+		  if(emm[3]==0xA4 && emm[0]==0x8F)
 		  {		    
 		    //cs_log("[cryptoworks-reader] EMM Dump: CMD: %s", cs_hexdump(1, emm+3, 5)); 
 		    //cs_log("[cryptoworks-reader] EMM Dump: DATA: %s",cs_hexdump(1, emm+8, emm[7]));
@@ -519,56 +523,43 @@ int cryptoworks_do_emm(EMM_PACKET *ep)
   	 	break;
 
   	 //GA    	 
-  	 case 0x88:
-  	 case 0x89:
-  	 	  if(emm[3]==0xA9 && emm[4]==0xFF && emm[8]==0x83 && emm[9]==0x01)
-		  {
-				insEMM_GA[4]=ep->emm[2]-2;
-				//cs_log("[cryptoworks-reader] EMM Dump: CMD: %s", cs_hexdump(1, insEMM_GA, 5)); 
-				//cs_log("[cryptoworks-reader] EMM Dump: DATA: %s",cs_hexdump(1, emm+5, insEMM_GA[4])); 				
-				//cs_log("[cryptoworks-reader] EMM Dump: IF: %02X == %02X",emm[7],(insEMM_GA[4]-3)); 								
-				
-				if(emm[7]==insEMM_GA[4]-3)
-				{
-					write_cmd(insEMM_GA, emm+5);
-					rc=((cta_res[0]==0x90)&&(cta_res[1]==0x00));					
-				}
-		  }
+  	 case GLOBAL:
+		insEMM_GA[4]=ep->emm[2]-2;
+		//cs_log("[cryptoworks-reader] EMM Dump: CMD: %s", cs_hexdump(1, insEMM_GA, 5)); 
+		//cs_log("[cryptoworks-reader] EMM Dump: DATA: %s",cs_hexdump(1, emm+5, insEMM_GA[4]));
+		//cs_log("[cryptoworks-reader] EMM Dump: IF: %02X == %02X",emm[7],(insEMM_GA[4]-3)); 
+		if(emm[7]==insEMM_GA[4]-3)
+		{
+			write_cmd(insEMM_GA, emm+5);
+			rc=((cta_res[0]==0x90)&&(cta_res[1]==0x00));					
+		}
   	 	break;
   	 
   	 //SA
-  	 case 0x84:
-  	 	  if(emm[3]==0xA9 && emm[4]==0xFF && emm[12]==0x80 && emm[13]==0x04)
-		  {
-				insEMM_SA[4]=ep->emm[2]-6;
-				//cs_log("[cryptoworks-reader] EMM Dump: CMD: %s", cs_hexdump(1, insEMM_SA, 5)); 
-				//cs_log("[cryptoworks-reader] EMM Dump: DATA: %s",cs_hexdump(1, emm+9, insEMM_SA[4])); 				
-				//cs_log("[cryptoworks-reader] EMM Dump: IF: %02X == %02X",emm[11],(insEMM_SA[4]-3)); 								
-				
-				if(emm[11]==insEMM_SA[4]-3)
-				{
-					write_cmd(insEMM_SA, emm+9);
-					rc=((cta_res[0]==0x90)&&(cta_res[1]==0x00));					
-				}
-		  }
+  	 case SHARED:
+		insEMM_SA[4]=ep->emm[2]-6;
+		//cs_log("[cryptoworks-reader] EMM Dump: CMD: %s", cs_hexdump(1, insEMM_SA, 5)); 
+		//cs_log("[cryptoworks-reader] EMM Dump: DATA: %s",cs_hexdump(1, emm+9, insEMM_SA[4]));
+		//cs_log("[cryptoworks-reader] EMM Dump: IF: %02X == %02X",emm[11],(insEMM_SA[4]-3)); 
+		if(emm[11]==insEMM_SA[4]-3)
+		{
+			write_cmd(insEMM_SA, emm+9);
+			rc=((cta_res[0]==0x90)&&(cta_res[1]==0x00));					
+		}
   	 	break;
   	 
   	 //UA	  	 
-  	 case 0x82:
-  	 	if(emm[3]==0xA9 && emm[4]==0xFF && emm[13]==0x80 && emm[14]==0x05)
+  	 case UNIQUE:
+		insEMM_UA[4]=ep->emm[2]-7;
+		//cs_log("[cryptoworks-reader] EMM Dump: CMD: %s", cs_hexdump(1, insEMM_UA, 5)); 
+		//cs_log("[cryptoworks-reader] EMM Dump: DATA: %s",cs_hexdump(1, emm+10, insEMM_UA[4])); 
+		//cs_log("[cryptoworks-reader] EMM Dump: IF: %02X == %02X",emm[12],(insEMM_UA[4]-3)); 
+		if(emm[12]==insEMM_UA[4]-3)
 		{
-			insEMM_UA[4]=ep->emm[2]-7;
-			//cs_log("[cryptoworks-reader] EMM Dump: CMD: %s", cs_hexdump(1, insEMM_UA, 5)); 
-			//cs_log("[cryptoworks-reader] EMM Dump: DATA: %s",cs_hexdump(1, emm+10, insEMM_UA[4])); 				
-			//cs_log("[cryptoworks-reader] EMM Dump: IF: %02X == %02X",emm[12],(insEMM_UA[4]-3)); 								
-			
-			if(emm[12]==insEMM_UA[4]-3)
-			{
-				//cryptoworks_send_pin(); //?? may be 
-				write_cmd(insEMM_UA, emm+10);
-				rc=((cta_res[0]==0x90)&&(cta_res[1]==0x00));					
-			}
-		}			
+			//cryptoworks_send_pin(); //?? may be 
+			write_cmd(insEMM_UA, emm+10);
+			rc=((cta_res[0]==0x90)&&(cta_res[1]==0x00));					
+		}
   	 	break;  	
   }
 
