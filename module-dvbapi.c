d42856b3cd33b7f3adfbe804cdf56267217a84a4
alno <alno@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-03-24 18:59:04 +0000

    some modifications for ecm.info parsing
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@1894 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-dvbapi.c
+++ module-dvbapi.c
@@ -1116,12 +1116,12 @@ void dvbapi_send_dcw(ECM_REQUEST *er) {
 			FILE *ecmtxt;
 			ecmtxt = fopen("/tmp/ecm.info", "w");
 			if(ecmtxt != NULL) {
-				fprintf(ecmtxt, "caid 0x%04X\npid 0x%04X\nprov 0x%06X\n", er->caid, er->pid, (uint) er->prid);
+				fprintf(ecmtxt, "caid: 0x%04X\npid: 0x%04X\nprov: 0x%06X\n", er->caid, er->pid, (uint) er->prid);
 				fprintf(ecmtxt, "reader: %s\n", reader[er->reader[0]].label);
 				if (reader[er->reader[0]].typ & R_IS_CASCADING)
 					fprintf(ecmtxt, "from: %s\n", reader[er->reader[0]].device);
 				fprintf(ecmtxt, "protocol: %d\n", reader[er->reader[0]].typ);
-				if (reader[er->reader[0]].typ == R_CCCAM)
+				//if (reader[er->reader[0]].typ == R_CCCAM)
 					fprintf(ecmtxt, "hops: %d\n", reader[er->reader[0]].cc_currenthops);
 				fprintf(ecmtxt, "ecm time: %.3f\n", (float) client[cs_idx].cwlastresptime/1000);
 				fprintf(ecmtxt, "cw0: %s\n", cs_hexdump(1,demux[i].lastcw[0],8));
