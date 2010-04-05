3bbf12a6828983711c6311a0bdb9c607f5f21779
alno <alno@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-04-05 12:14:49 +0000

    fix typo
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@1956 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam.c
+++ oscam.c
@@ -2251,7 +2251,7 @@ void do_emm(EMM_PACKET *ep)
 	ep->cidx = cs_idx;
 	cs_debug_mask(D_EMM, "emm is being sent to reader %s.", reader[au].label);
 	write_to_pipe(reader[au].fd, PIP_ID_EMM, (uchar *) ep, sizeof(EMM_PACKET));
-	if (reader[au].type == R_IS_NETWORK)
+	if (reader[au].typ & R_IS_NETWORK)
 		cs_log("emm written to %s type=%s len=%d", reader[au].label, typtext[ep->type], ep->l);
 }
 
