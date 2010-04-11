d2a0c67142083b54a99195dd6f7bc8c52ce190fe
merek <merek@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-04-11 10:22:05 +0000

    Merge from UMP r2013
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@2046 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-serial.c
+++ module-serial.c
@@ -560,7 +560,7 @@ static void oscam_ser_disconnect()
 
 static void oscam_ser_auth_client(int proto)
 {
-  int ok=0;
+  int ok; //initializing this variable to 0 does prevent reconnect
   // After reload base account ptrs may be placed in other address,
   // and we may can't find it in this process. 
   // Simply save valid account.
