832f8860859bfee15355311c553ac2e937a0dcaf
merek <merek@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-04-11 11:07:38 +0000

    Fix typo
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@2051 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- reader-dre.c
+++ reader-dre.c
@@ -307,6 +307,7 @@ int dre_get_emm_type(EMM_PACKET *ep, struct s_reader * rdr)
 				memset(ep->hexserial, 0, 8);
 				memcpy(ep->hexserial, ep->emm + 3, 4);
 				return (!memcmp(&rdr->sa[0][0], ep->emm + 3, 4));
+			}
 			else
 				return TRUE;
 		default:
