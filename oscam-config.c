b7de4d5df30a54afcefcfe56c11e6222b6880014
alno <alno@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-07-04 21:55:17 +0000

    WebIf: fix typo
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@2643 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam-config.c
+++ oscam-config.c
@@ -1929,9 +1929,10 @@ int write_config()
         	dot = "";
         	while(cfg->dvbapi_prioritytab.caid[i]) {
         		fprintf(f, "%s%04X", dot, cfg->dvbapi_prioritytab.caid[i]);
-        		if(cfg->dvbapi_prioritytab.mask[i])
+        		if(cfg->dvbapi_prioritytab.mask[i]) {
         			provid = (cfg->dvbapi_prioritytab.cmap[i] << 8 | cfg->dvbapi_prioritytab.mask[i]);
         			fprintf(f, ":%06lX", provid);
+        		}
         		dot = ",";
         		i++;
         	}
@@ -1945,9 +1946,10 @@ int write_config()
         	dot = "";
         	while(cfg->dvbapi_ignoretab.caid[i]) {
         		fprintf(f, "%s%04X", dot, cfg->dvbapi_ignoretab.caid[i]);
-        		if(cfg->dvbapi_ignoretab.mask[i])
+        		if(cfg->dvbapi_ignoretab.mask[i]) {
         			provid = (cfg->dvbapi_ignoretab.cmap[i] << 8 | cfg->dvbapi_ignoretab.mask[i]);
         			fprintf(f, ":%06lX", provid);
+        		}
         		dot = ",";
         		i++;
         	}
--- oscam-http.c
+++ oscam-http.c
@@ -546,9 +546,10 @@ void send_oscam_config_dvbapi(struct templatevars *vars, FILE *f, struct uripara
 	ulong provid = 0;
 	while(cfg->dvbapi_prioritytab.caid[i]) {
 		tpl_printf(vars, 1, "PRIORITY", "%s%04X", dot, cfg->dvbapi_prioritytab.caid[i]);
-		if(cfg->dvbapi_prioritytab.mask[i])
+		if(cfg->dvbapi_prioritytab.mask[i]){
 			provid = (cfg->dvbapi_prioritytab.cmap[i] << 8 | cfg->dvbapi_prioritytab.mask[i]);
 			tpl_printf(vars, 1, "PRIORITY", ":%06lX", provid);
+		}
 		dot = ",";
 		i++;
 	}
@@ -558,9 +559,10 @@ void send_oscam_config_dvbapi(struct templatevars *vars, FILE *f, struct uripara
 	provid = 0;
 	while(cfg->dvbapi_ignoretab.caid[i]) {
 		tpl_printf(vars, 1, "IGNORE", "%s%04X", dot, cfg->dvbapi_ignoretab.caid[i]);
-		if(cfg->dvbapi_ignoretab.mask[i])
+		if(cfg->dvbapi_ignoretab.mask[i]) {
 			provid = (cfg->dvbapi_ignoretab.cmap[i] << 8 | cfg->dvbapi_ignoretab.mask[i]);
 			tpl_printf(vars, 1, "IGNORE", ":%06lX", provid);
+		}
 		dot = ",";
 		i++;
 	}
