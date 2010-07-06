afccbffbef7615a1adb445df9d5cbf2eea6761f8
_network <_network@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-07-06 18:30:17 +0000

    dvbapi: skip ecmpid on read error
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@2664 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-dvbapi.c
+++ module-dvbapi.c
@@ -308,9 +308,11 @@ int dvbapi_read_device(int dmx_fd, unsigned char *buf, int length) {
 	pfd[0].fd = dmx_fd;
 	pfd[0].events = (POLLIN | POLLPRI);
 
-	rc = poll(pfd, 1, 1000);
-	if (rc<1)
+	rc = poll(pfd, 1, 2000);
+	if (rc<1) {
+		cs_log("read on %d timed out", dmx_fd);
 		return -1;
+	}
 
 	len = read(dmx_fd, buf, length);
 
@@ -1240,11 +1242,15 @@ void dvbapi_main_local() {
 					dvbapi_handlesockmsg(mbuf, len, connfd);
 
 				} else { // type==0
-					if ((len=dvbapi_read_device(pfd2[i].fd, mbuf, sizeof(mbuf))) <= 0)
-						continue;
-
 					int demux_index=ids[i];
 					int n=fdn[i];
+
+					if ((len=dvbapi_read_device(pfd2[i].fd, mbuf, sizeof(mbuf))) <= 0) {
+						if (demux[demux_index].pidindex==-1) {
+							dvbapi_try_next_caid(demux_index);
+						}
+						continue;
+					}
 
 					if (pfd2[i].fd==(int)demux[demux_index].demux_fd[n].fd) {
 						dvbapi_process_input(demux_index,n,mbuf,len);
