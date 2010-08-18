cbf559fe284f511027b69ae82c5cde4ae95d7c0c
rorothetroll <rorothetroll@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-08-18 17:24:54 +0000

    remove the test for TA1==0 as some conax card do have TA1 set to 00
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@2931 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- csctapi/atr.c
+++ csctapi/atr.c
@@ -98,6 +98,8 @@ int ATR_InitFromArray (ATR * atr, BYTE atr_buffer[ATR_MAX_SIZE], unsigned length
 		if ((TDi | 0xEF) == 0xFF)
 		{
 			pointer++;
+			cs_log("ta = %02X", buffer[pointer]);
+			cs_log("pn = %02X", pn);
 			atr->ib[pn][ATR_INTERFACE_BYTE_TA].value = buffer[pointer];
 			atr->ib[pn][ATR_INTERFACE_BYTE_TA].present = TRUE;
 		}
@@ -183,13 +185,10 @@ int ATR_InitFromArray (ATR * atr, BYTE atr_buffer[ATR_MAX_SIZE], unsigned length
     // check that TA1, if pn==1 , has a valid value for FI
     if ( (atr->pn==1) && (atr->ib[pn][ATR_INTERFACE_BYTE_TA].present = TRUE)) {
         uchar FI;
+        cs_debug("TA1 = %02x",atr->ib[pn][ATR_INTERFACE_BYTE_TA].value);
         FI=(atr->ib[pn][ATR_INTERFACE_BYTE_TA].value & 0xF0)>>4;
+        cs_debug("FI = %02x",FI);
         if(atr_fs_table[FI]==0) {
-            cs_debug("Invalid ATR as FI is not returning a valid frequency value");
-            return (ATR_MALFORMED);
-        }
-
-        if (atr->ib[pn][ATR_INTERFACE_BYTE_TA].value == 0) {
             cs_debug("Invalid ATR as FI is not returning a valid frequency value");
             return (ATR_MALFORMED);
         }
