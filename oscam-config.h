41d1ecb5a5461d069c227c5b4006e0640a8dd9fb
_network <_network@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-10-03 08:45:57 +0000

    fix #975
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@3352 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam-config.h
+++ oscam-config.h
@@ -1,40 +1,117 @@
 #ifndef OSCAM_CONFIG_H_
 #define OSCAM_CONFIG_H_
 
+// ADDONS
+#ifndef WEBIF
+//#define WEBIF
+#endif
+
 #ifndef HAVE_DVBAPI
 #if !defined(OS_CYGWIN32) && !defined(OS_HPUX) && !defined(OS_FREEBSD) && !defined(OS_MACOSX)
-#define HAVE_DVBAPI
+//#define HAVE_DVBAPI
 #endif
 #endif
 
+#ifndef IRDETO_GUESSING
+//#define IRDETO_GUESSING
+#endif
+
+#ifndef CS_ANTICASC
 #define CS_ANTICASC
+#endif
 
+#ifndef WITH_DEBUG
 #define WITH_DEBUG
+#endif
+
+#ifndef CS_LED
+//#define CS_LED
+#endif
+
+#ifndef CS_WITH_DOUBLECHECK
+//#define CS_WITH_DOUBLECHECK
+#endif
 
 // MODULE
+#ifndef MODULE_MONITOR
 #define MODULE_MONITOR
+#endif
+
+#ifndef MODULE_CAMD33
 #define MODULE_CAMD33
+#endif
+
+#ifndef MODULE_CAMD35
 #define MODULE_CAMD35
+#endif
+
+#ifndef MODULE_CAMD35_TCP
 #define MODULE_CAMD35_TCP
+#endif
+
+#ifndef MODULE_NEWCAMD
 #define MODULE_NEWCAMD
+#endif
+
+#ifndef MODULE_CCCAM
 #define MODULE_CCCAM
+#endif
+
+#ifndef MODULE_RADEGAST
 #define MODULE_RADEGAST
+#endif
+
+#ifndef MODULE_SERIAL
 #define MODULE_SERIAL
+#endif
+
+#ifndef MODULE_CONSTCW
 #define MODULE_CONSTCW
+#endif
+
 
 // CARDREADER
+#ifndef WITH_CARDREADER
 #define WITH_CARDREADER
+#endif
+
 
 #ifdef WITH_CARDREADER
+#ifndef READER_NAGRA
 #define READER_NAGRA
+#endif
+
+#ifndef READER_IRDETO
 #define READER_IRDETO
+#endif
+
+#ifndef READER_CONAX
 #define READER_CONAX
+#endif
+
+#ifndef READER_CRYPTOWORKS
 #define READER_CRYPTOWORKS
+#endif
+
+#ifndef READER_SECA
 #define READER_SECA
+#endif
+
+#ifndef READER_VIACCESS
 #define READER_VIACCESS
+#endif
+
+#ifndef READER_VIDEOGUARD
 #define READER_VIDEOGUARD
+#endif
+
+#ifndef READER_DRE
 #define READER_DRE
+#endif
+
+#ifndef READER_TONGFANG
 #define READER_TONGFANG
+#endif
 #endif
 
 #define CS_LOGHISTORY
