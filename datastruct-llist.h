05366b73bb8c8082e938434d32688fa8a9e1cd97
cogsi <cogsi@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-10-17 10:48:02 +0000

    LLIST updated
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@3584 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- datastruct-llist.h
+++ datastruct-llist.h
@@ -1,39 +1,46 @@
 
 /* singularly linked-list */
 
-typedef struct llnode LLNODE;
+#ifndef DATASTRUCT_LLIST_H_
+#define DATASTRUCT_LLIST_H_
+
+#include <pthread.h>
+
+typedef struct llnode LL_NODE;
 struct llnode {
     void *obj;
-    LLNODE *nxt;
+    LL_NODE *nxt, *prv;
 };
 
 typedef struct llist LLIST;
 struct llist {
     void *obj;
-    LLNODE *initial;
+    LL_NODE *initial;
     pthread_mutex_t lock;
 };
 
-typedef struct lliter LLITER;
+typedef struct lliter LL_ITER;
 struct lliter {
     LLIST *l;
-    LLNODE *current;
+    LL_NODE *cur;
 };
 
-LLIST *ll_create();			// create llist, return ptr to llist
-void ll_destroy_abstract(LLIST *l);	// same as ll_clear_abstract() but frees up LLIST mem as well
-void ll_destroy_data(LLIST *l);		// same as ll_clear_data() but frees up obj allocations as well
-void ll_clear_abstract(LLIST *l);	// frees up all llnodes nodes but not data held in obj ptrs
-void ll_clear_data(LLIST *l);		// same as ll_clear_data() but frees up obj allocations as well
+LLIST *ll_create();             // create llist, return ptr to llist
+void ll_destroy(LLIST *l);      // same as ll_clear_abstract() but frees up LLIST mem as well
+void ll_destroy_data(LLIST *l); // same as ll_clear_data() but frees up obj allocations as well
+void ll_clear(LLIST *l);        // frees up all llnodes nodes but not data held in obj ptrs
+void ll_clear_data(LLIST *l);   // same as ll_clear_data() but frees up obj allocations as well
 
 void ll_append(LLIST *l, void *obj);    // append obj to llist
 
-LLITER *ll_iter_create(LLIST *l);	// return ptr to iterator obj
-void ll_iter_release(LLITER *it);       // free up the iterator obj
-void *ll_iter_next(LLITER *it);         // iterate to and return next llnode obj, returns NULL at end
-void ll_iter_reset(LLITER *it);         // reset itrerator to first llnode
-void ll_iter_insert(LLITER *it, void *obj);    // insert obj at iterator node
-void *ll_iter_remove(LLITER *it);       // remove llnode at iterator, returns ptr to obj removed
-void ll_iter_remove_data(LLITER *it);   // remove llnode and free llnode obj
+LL_ITER *ll_iter_create(LLIST *l);              // return ptr to iterator obj
+void ll_iter_release(LL_ITER *it);              // free up the iterator obj
+void *ll_iter_next(LL_ITER *it);                // iterate to and return next llnode obj, returns NULL at end
+void ll_iter_reset(LL_ITER *it);                // reset itrerator to first llnode
+void ll_iter_insert(LL_ITER *it, void *obj);    // insert obj at iterator node
+void *ll_iter_remove(LL_ITER *it);              // remove llnode at iterator, returns ptr to the llnode obj removed
+void ll_iter_remove_data(LL_ITER *it);          // remove llnode and free llnode obj
 
 int ll_count(LLIST *l);                 // return number of items in list
+
+#endif
