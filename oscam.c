06d3210b5ae9d3105ea8a0a467106d47ed2892c3
merek <merek@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-10-25 11:21:27 +0000

    more fixes for cs_betatunnel
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@3722 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam.c
+++ oscam.c
@@ -1870,22 +1870,22 @@ void cs_betatunnel(ECM_REQUEST *er)
 	ttab = &cl->ttab;
 	for (n = 0; (n < CS_MAXTUNTAB); n++) {
 		if ((er->caid==ttab->bt_caidfrom[n]) && ((er->srvid==ttab->bt_srvid[n]) || (ttab->bt_srvid[n])==mask_all)) {
-			uchar hack_n3[13] = {0x30, 0x51, 0xc7, 0x00, 0x00, 0x00, 0x01, 0x10, 0x10, 0x00, 0x87, 0x12, 0x07};
-			uchar hack_n2[13] = {0x70, 0x51, 0xc9, 0x00, 0x00, 0x00, 0x01, 0x10, 0x10, 0x00, 0x48, 0x12, 0x07};
+			uchar hack_n3[11] = {0xc7, 0x00, 0x00, 0x00, 0x01, 0x10, 0x10, 0x00, 0x87, 0x12, 0x07};
+			uchar hack_n2[11] = {0xc9, 0x00, 0x00, 0x00, 0x01, 0x10, 0x10, 0x00, 0x48, 0x12, 0x07};
 			er->caid = ttab->bt_caidto[n];
 			er->prid = 0;
-			er->l = (er->ecm[2]+3);
-			memmove(er->ecm+14, er->ecm+4, er->l-1);
+			er->l = er->ecm[2] + 3;
+			memmove(er->ecm + 14, er->ecm + 4, er->l);
 			if (er->l > 0x88) {
-				memcpy(er->ecm+1, hack_n3, 13);
+				memcpy(er->ecm + 3, hack_n3, 11);
 				if (er->ecm[0] == 0x81)
 					er->ecm[12] += 1;
 			}
 			else {
-				memcpy(er->ecm+1, hack_n2, 13);
+				memcpy(er->ecm + 3, hack_n2, 11);
 			}
-			er->l += 10;
-			er->ecm[2] = er->l-3;
+                        er->l += 10;
+			er->ecm[2] = er->l - 3;
 			er->btun = 1;
 			cl->cwtun++;
 			cs_debug("ECM converted from: 0x%X to BetaCrypt: 0x%X for service id:0x%X",
