8392168c69edbeb1f9714181cb81e513ae84eb1b
alno <alno@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-11-07 13:23:13 +0000

    Monitor: fix crash on send 'log' w/o arg
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@3845 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-monitor.c
+++ module-monitor.c
@@ -460,6 +460,8 @@ static void monitor_logsend(char *flag){
 #ifdef CS_LOGHISTORY
 	int i;
 #endif
+	if (!flag[0]) return; //no arg
+
 	if (strcmp(flag, "on")) {
 		if (strcmp(flag, "onwohist")) {
 			cur_client()->log=0;
