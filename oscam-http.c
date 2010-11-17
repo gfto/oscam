18205e1040875753e2b2d58bf6b6b9af60ec3527
alno <alno@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-11-17 00:00:21 +0000

    WebIf: add re-read for 'oscam.dvbapi' on saving in FILES section of webIf. So after saving new filters are active. THX @ _network for preparation
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@3910 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam-http.c
+++ oscam-http.c
@@ -845,7 +845,7 @@ void send_oscam_reader_config(struct templatevars *vars, FILE *f, struct uripara
 					if(strlen((*params).values[i]) > 0)
 						chk_reader((*params).params[i], (*params).values[i], rdr);
 			}
-			//printf("param %s value %s\n",(*params).params[i], (*params).values[i]);
+			printf("param %s value %s\n",(*params).params[i], (*params).values[i]);
 		}
 		chk_reader("services", servicelabels, rdr);
 
@@ -2364,6 +2364,11 @@ void send_oscam_files(struct templatevars *vars, FILE *f, struct uriparams *para
 				if((fpsave = fopen(targetfile,"w"))){
 					fprintf(fpsave,"%s",fcontent);
 					fclose(fpsave);
+
+#ifdef HAVE_DVBAPI
+					 if (strcmp(getParam(params, "part"), "dvbapi") == 0)
+						 dvbapi_read_priority()
+#endif
 				}
 			}
 		}
