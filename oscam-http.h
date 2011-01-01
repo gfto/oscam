c5068bc41a6f107a046a7b2cff5eb66e927457cd
alno <alno@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2011-01-01 16:04:28 +0000

    WebIf: fix preselection 4 in pmt_mode for dvbApi
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@4278 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam-http.h
+++ oscam-http.h
@@ -1344,7 +1344,7 @@ totalblocked=\"##TOTALBLOCKED##\" totalerror=\"##TOTALERROR##\">\n##EMMSTATS##\
 			<option value=\"1\" ##PMTMODESELECTED1##>1 - disable reading PMT file</option>\n\
 			<option value=\"2\" ##PMTMODESELECTED2##>2 - disable camd.socket</option>\n\
 			<option value=\"3\" ##PMTMODESELECTED3##>3 - read PMT file on startup only</option>\n\
-			<option value=\"4\" ##PMTMODESELECTED3##>4 - ufs922 with new libioctl.so</option>\n\
+			<option value=\"4\" ##PMTMODESELECTED4##>4 - ufs922 with new libioctl.so</option>\n\
 		</SELECT></TD></TR>\n\
     <TR><TD colspan=\"2\" align=\"right\"><input type=\"submit\" value=\"OK\" ##BTNDISABLED##>\n</TD></TR>\n\
 	</TABLE>\n\
