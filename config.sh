0e56cefdcc63fe77cc25b1bb847f1357288c3b5c
landlord <landlord@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2011-03-03 19:53:46 +0000

    Updated 'config.sh'.
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@4769 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@ width=65
 listheight=10
 
 if [ -z "${DIALOG}" ]; then
-	echo "Please install the dialog package" 1>&2
+	echo "Please install dialog package." 1>&2
 	exit 1
 fi
 
@@ -28,7 +28,6 @@ disable_all() {
 	for i in $1; do
 		sed -i -e "s/^#define ${i}$/\/\/#define ${i}/g" $configfile
 	done
-
 }
 
 enable_package() {
@@ -41,34 +40,35 @@ enable_package() {
 print_components() {
 	clear
 	echo "You have selected the following components:"
-	echo -e "\nAddons:"
+	echo -e "\nadd-ons:"
 	for i in $addons; do
 		printf "\t%-20s: %s\n" $i $(check_test "$i")
 	done
 
-	echo -e "\nProtocols:"
+	echo -e "\nprotocols:"
 	for i in $protocols; do
 		printf "\t%-20s: %s\n" $i $(check_test "$i")
 	done
 
-	echo -e "\nReaders:"
+	echo -e "\nreaders:"
 	for i in $readers; do
 		printf "\t%-20s: %s\n" $i $(check_test "$i")
 	done
 }
 
 menu_addons() {
-	${DIALOG} --checklist "\nChoose Addons:\n " $height $width $listheight \
-		WEBIF			"Webinterface"	$(check_test "WEBIF") \
-		HAVE_DVBAPI		"DVB-API"		$(check_test "HAVE_DVBAPI") \
-		IRDETO_GUESSING	"Irdeto Guessing"	$(check_test "IRDETO_GUESSING") \
-		CS_ANTICASC		"Anti Cascadingg"	$(check_test "CS_ANTICASC") \
+	${DIALOG} --checklist "\nChoose add-ons:\n " $height $width $listheight \
+		WEBIF			"Web interface"		$(check_test "WEBIF") \
+		HAVE_DVBAPI		"DVB API"		$(check_test "HAVE_DVBAPI") \
+		IRDETO_GUESSING		"Irdeto guessing"	$(check_test "IRDETO_GUESSING") \
+		CS_ANTICASC		"Anti cascading"	$(check_test "CS_ANTICASC") \
 		WITH_DEBUG		"Debug messages"	$(check_test "WITH_DEBUG") \
-		CS_WITH_DOUBLECHECK	"WITH_DOUBLECHECK"	$(check_test "CS_WITH_DOUBLECHECK") \
+		CS_WITH_DOUBLECHECK	"ECM doublecheck"	$(check_test "CS_WITH_DOUBLECHECK") \
 		CS_LED			"LED"			$(check_test "CS_LED") \
 		QBOXHD_LED		"QboxHD LED"		$(check_test "QBOXHD_LED") \
-		CS_LOGHISTORY		"Log History"		$(check_test "CS_LOGHISTORY") \
-		WITH_SSL		"Use OpenSSL"		$(check_test "WITH_SSL") \
+		CS_LOGHISTORY		"Log history"		$(check_test "CS_LOGHISTORY") \
+		MODULE_MONITOR		"Monitor"		$(check_test "MODULE_MONITOR") \
+		WITH_SSL		"OpenSSL support"	$(check_test "WITH_SSL") \
 		2> ${tempfile}
 
 	opt=${?}
@@ -79,17 +79,16 @@ menu_addons() {
 }
 
 menu_protocols() {
-	${DIALOG} --checklist "\nChoose Protocols:\n " $height $width $listheight \
-		MODULE_CAMD33		"Camd3.3"	$(check_test "MODULE_CAMD33") \
-		MODULE_CAMD35		"Camd3.5"	$(check_test "MODULE_CAMD35") \
-		MODULE_CAMD35_TCP	"Camd3.5 TCP"	$(check_test "MODULE_CAMD35_TCP") \
-		MODULE_NEWCAMD	"Newcamd"	$(check_test "MODULE_NEWCAMD") \
-		MODULE_CCCAM		"CCCam"	$(check_test "MODULE_CCCAM") \
-		MODULE_GBOX		"Gbox"	$(check_test "MODULE_GBOX") \
-		MODULE_RADEGAST	"Radegast"	$(check_test "MODULE_RADEGAST") \
-		MODULE_SERIAL		"Serial"	$(check_test "MODULE_SERIAL") \
-		MODULE_MONITOR	"Monitor"	$(check_test "MODULE_MONITOR") \
-		MODULE_CONSTCW	"Constcw"	$(check_test "MODULE_CONSTCW") \
+	${DIALOG} --checklist "\nChoose protocols:\n " $height $width $listheight \
+		MODULE_CAMD33		"camd 3.3"	$(check_test "MODULE_CAMD33") \
+		MODULE_CAMD35		"camd 3.5"	$(check_test "MODULE_CAMD35") \
+		MODULE_CAMD35_TCP	"camd 3.5 TCP"	$(check_test "MODULE_CAMD35_TCP") \
+		MODULE_NEWCAMD		"newcamd"	$(check_test "MODULE_NEWCAMD") \
+		MODULE_CCCAM		"CCcam"		$(check_test "MODULE_CCCAM") \
+		MODULE_GBOX		"gbox"		$(check_test "MODULE_GBOX") \
+		MODULE_RADEGAST		"radegast"	$(check_test "MODULE_RADEGAST") \
+		MODULE_SERIAL		"serial"	$(check_test "MODULE_SERIAL") \
+		MODULE_CONSTCW		"constant CW"	$(check_test "MODULE_CONSTCW") \
 		2> ${tempfile}
 
 	opt=${?}
@@ -100,15 +99,15 @@ menu_protocols() {
 }
 
 menu_reader() {
-	${DIALOG} --checklist "\nChoose Reader:\n " $height $width $listheight \
-		READER_NAGRA		"Nagravision"	$(check_test "READER_NAGRA") \
-		READER_IRDETO		"Irdeto"	$(check_test "READER_IRDETO") \
-		READER_CONAX		"Conax"	$(check_test "READER_CONAX") \
-		READER_CRYPTOWORKS	"Cryptoworks"	$(check_test "READER_CRYPTOWORKS") \
-		READER_SECA		"Seca"		$(check_test "READER_SECA") \
-		READER_VIACCESS	"Viaccess"	$(check_test "READER_VIACCESS") \
-		READER_VIDEOGUARD	"Videoguard"	$(check_test "READER_VIDEOGUARD") \
-		READER_DRE		"Dre"		$(check_test "READER_DRE") \
+	${DIALOG} --checklist "\nChoose reader:\n " $height $width $listheight \
+		READER_NAGRA		"Nagravision"		$(check_test "READER_NAGRA") \
+		READER_IRDETO		"Irdeto"		$(check_test "READER_IRDETO") \
+		READER_CONAX		"Conax"			$(check_test "READER_CONAX") \
+		READER_CRYPTOWORKS	"Cryptoworks"		$(check_test "READER_CRYPTOWORKS") \
+		READER_SECA		"Seca"			$(check_test "READER_SECA") \
+		READER_VIACCESS		"Viaccess"		$(check_test "READER_VIACCESS") \
+		READER_VIDEOGUARD	"NDS Videoguard"	$(check_test "READER_VIDEOGUARD") \
+		READER_DRE		"DRE Crypt"		$(check_test "READER_DRE") \
 		READER_TONGFANG		"Tongfang"		$(check_test "READER_TONGFANG") \
 		2> ${tempfile}
 
@@ -125,9 +124,9 @@ menu_reader() {
 
 while true; do
 	${DIALOG} --menu "\nSelect category:\n " $height $width $listheight \
-		Addons		"Addons" \
-		Protocols	"Network Protocols" \
-		Reader		"Card Reader" \
+		Add-ons		"Add-ons" \
+		Protocols	"Network protocols" \
+		Reader		"Reader" \
 		Quit		"Quit" \
 		2> ${tempfile}
 
@@ -136,7 +135,7 @@ while true; do
 
 	menuitem=`cat $tempfile`
 	case $menuitem in
-       	Addons) menu_addons;;
+		Add-ons) menu_addons;;
 		Protocols) menu_protocols;;
 		Reader) menu_reader;;
 		Quit) rm $tempfile; print_components; exit;;
