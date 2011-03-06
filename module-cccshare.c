89f0729c229350d6de3b7db25e8fda020f45b8be
schlocke <schlocke@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2011-03-06 10:01:26 +0000

    cccam: fixed segfault/problem from [4793]
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@4794 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-cccshare.c
+++ module-cccshare.c
@@ -112,7 +112,7 @@ int send_card_to_clients(struct cc_card *card, struct s_client *one_client) {
         struct s_client *cl;
         for (cl = one_client?one_client:first_client; cl; cl=one_client?NULL:cl->next) {
                 struct cc_data *cc = cl->cc;
-                if (ph[cl->ctyp].num == R_CCCAM && cc && cc->mode == CCCAM_MODE_NORMAL) { //CCCam-Client!
+                if (cl->typ=='c' && cc && (one_client || (ph[cl->ctyp].num == R_CCCAM && cc->mode == CCCAM_MODE_NORMAL))) { //CCCam-Client!
                         if (card_valid_for_client(cl, card)) {
 								int usr_reshare = cl->account->cccreshare;
                                 int usr_ignorereshare = cl->account->cccignorereshare;
