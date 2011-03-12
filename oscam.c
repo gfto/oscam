22f4c2e6b0eb9c255c00d046e8772e71a03c8f55
schlocke <schlocke@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2011-03-12 18:31:12 +0000

    duplicate user logging improved
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@4802 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam.c
+++ oscam.c
@@ -1226,12 +1226,14 @@ static void cs_fake_client(struct s_client *client, char *usr, int uniq, in_addr
 		if (cl != client && (cl->typ == 'c') && !cl->dup && !strcmp(cl->account->usr, usr)
 		   && (uniq < 5) && ((uniq % 2) || (cl->ip != ip)))
 		{
+		        char buf[20];
 			if (uniq  == 3 || uniq == 4)
 			{
 				cl->dup = 1;
 				cl->aureader_list = NULL;
+				strcpy(buf, cs_inet_ntoa(cl->ip));
 				cs_log("client(%8X) duplicate user '%s' from %s (prev %s) set to fake (uniq=%d)", 
-					cl->thread, usr, cs_inet_ntoa(ip), cs_inet_ntoa(cl->ip), uniq);
+					cl->thread, usr, cs_inet_ntoa(ip), buf, uniq);
 				if (cl->failban & BAN_DUPLICATE) {
 					cs_add_violation(cl->ip);
 				}
@@ -1240,8 +1242,9 @@ static void cs_fake_client(struct s_client *client, char *usr, int uniq, in_addr
 			{
 				client->dup = 1;
 				client->aureader_list = NULL;
+				strcpy(buf, cs_inet_ntoa(ip));
 				cs_log("client(%8X) duplicate user '%s' from %s (prev %s) set to fake (uniq=%d)", 
-					pthread_self(), usr, cs_inet_ntoa(cl->ip), cs_inet_ntoa(ip), uniq);
+					pthread_self(), usr, cs_inet_ntoa(cl->ip), buf, uniq);
 				if (client->failban & BAN_DUPLICATE) {
 					cs_add_violation(ip);
 				}
