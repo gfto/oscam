33cc36215f7df64b23eeebd660ac3bbf39ed7548
schlocke <schlocke@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2011-04-04 07:12:54 +0000

    LLIST: added more locks to get threadsafer
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@4908 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-datastruct-llist.c
+++ module-datastruct-llist.c
@@ -6,6 +6,15 @@
 #include "globals.h"
 #include "module-datastruct-llist.h"
 
+/*
+  Locking rules:
+  
+  mutex lock is needed when...
+  1. l->initial is modified/accessed
+  2. LL_NODE nxt modified/accessed
+
+
+*/
 static void _destroy(LLIST *l)
 {
     if (!l) return;
@@ -58,11 +67,10 @@ void ll_clear_data(LLIST *l)
     l->count = 0;
 }
 
-LL_NODE* ll_append(LLIST *l, void *obj)
+LL_NODE* ll_append_nolock(LLIST *l, void *obj)
 {
     if (l && obj) {
         LL_NODE *new = calloc(1, sizeof(LL_NODE));
-        pthread_mutex_lock(&l->lock);
         LL_NODE *n = l->initial;
 
         new->obj = obj;
@@ -74,11 +82,20 @@ LL_NODE* ll_append(LLIST *l, void *obj)
             l->initial = new;
     
         l->count++;
-        pthread_mutex_unlock(&l->lock);
-
         return new;
     }
 
+    return NULL;
+}
+
+LL_NODE* ll_append(LLIST *l, void *obj)
+{
+    if (l && obj) {
+        pthread_mutex_lock(&l->lock);
+        LL_NODE *n = ll_append_nolock(l, obj);
+        pthread_mutex_unlock(&l->lock);
+        return n;
+    }
     return NULL;
 }
 
@@ -124,9 +141,9 @@ void ll_iter_release(LL_ITER *it)
     add_garbage(it);
 }
 
-void *ll_iter_next(LL_ITER *it)
+void *ll_iter_next_nolock(LL_ITER *it)
 {
-    if (it) {
+    if (it && it->l) {
         if (it->cur) {
             it->prv = it->cur;
             it->cur = it->cur->nxt;
@@ -140,21 +157,37 @@ void *ll_iter_next(LL_ITER *it)
     return NULL;
 }
 
+void *ll_iter_next(LL_ITER *it)
+{
+    if (it && it->l) {
+    	pthread_mutex_lock(&it->l->lock);
+    	void *res = ll_iter_next_nolock(it);
+		pthread_mutex_unlock(&it->l->lock);
+		return res;
+    }
+    return NULL;
+}
+
 void *ll_iter_peek(LL_ITER *it, int offset)
 {
-    LL_NODE *n = it->cur;
-    int i;
+	if (it && it->l) {
+		pthread_mutex_lock(&it->l->lock);
+	    LL_NODE *n = it->cur;
+	    int i;
 
-    for (i = 0; i < offset; i++)
-        if (n)
-            n = n->nxt;
-        else
-            return NULL;
-
-    if (!n)
-      return NULL;
-      
-    return n->obj;
+	    for (i = 0; i < offset; i++) {
+	    	if (n)
+            	n = n->nxt;
+			else 
+				break;
+		}
+		pthread_mutex_unlock(&it->l->lock);
+	    
+		if (!n)
+			return NULL;
+		return n->obj;
+	}
+	return NULL;
 }
 
 void ll_iter_reset(LL_ITER *it)
@@ -168,10 +201,10 @@ void ll_iter_reset(LL_ITER *it)
 void ll_iter_insert(LL_ITER *it, void *obj)
 {
     if (it && obj) {
+	   	pthread_mutex_lock(&it->l->lock);
         if (!it->cur || !it->cur->nxt)
-            ll_append(it->l, obj);
+            ll_append_nolock(it->l, obj);
         else {
-		   	pthread_mutex_lock(&it->l->lock);
             LL_NODE *n = calloc(1, sizeof(LL_NODE));
 
             n->obj = obj;
@@ -179,8 +212,8 @@ void ll_iter_insert(LL_ITER *it, void *obj)
             it->cur->nxt = n;
 
             it->l->count++;
-            pthread_mutex_unlock(&it->l->lock);
         }
+        pthread_mutex_unlock(&it->l->lock);
     }
 }
 
@@ -201,7 +234,7 @@ void *ll_iter_remove(LL_ITER *it)
             it->l->count--;
 
             ll_iter_reset(it);
-            while (prv && ll_iter_next(it))
+            while (prv && ll_iter_next_nolock(it))
                 if (it->cur == prv)
                     break;
 
