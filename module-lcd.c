0461c04e745e910b41d6f43394583d9f108e9b05
alno <alno@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2011-05-25 18:15:12 +0000

    LCD: some corrections and removing windows linebreaks
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@5290 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-lcd.c
+++ module-lcd.c
@@ -18,59 +18,96 @@ int8_t running;
 
 void refresh_lcd_file() {
 
-	int16_t cnt = 0, idx = 0, count_r = 0, count_p = 0, count_u = 0;
+
 	char targetfile[256];
 	snprintf(targetfile, sizeof(targetfile),"%s%s", get_tmp_dir(), "/oscam.lcd");
 
+	int32_t seconds = 0, secs = 0, fullmins = 0, mins = 0, fullhours = 0, hours = 0,	days = 0;
+
 	while(running) {
 
+		int16_t cnt = 0, idx = 0, count_r = 0, count_p = 0, count_u = 0;
 		FILE *fpsave;
 
 		if((fpsave = fopen(targetfile,"w"))){
 
-			struct s_reader *rdr;
-			LL_ITER itr = ll_iter_create(configured_readers);
-			while((rdr = ll_iter_next(&itr))){
-				fprintf(fpsave,"reader%d %s\n", idx, rdr->label);
-
-				if(rdr->typ & R_IS_NETWORK){
-					if (rdr->card_status == CARD_INSERTED)
-						fprintf(fpsave,"reader%d %s\n", idx, "CONNECTED");
-					count_p++;
-				} else {
-					if (rdr->card_status == CARD_INSERTED)
-						fprintf(fpsave,"reader%d %s\n", idx, "CARDOK");
-					count_r++;
-				}
-
-				idx++;
-			}
-
 			idx = 0;
 			int16_t i;
+			char *type = "---";
+			char *label = "+++";
+			char *status = "xxx";
+
 			struct s_client *cl;
 			for ( i=0, cl=first_client; cl ; cl=cl->next, i++) {
-				if (cl->typ=='c'){
-					count_u++;
+				type = "---";
+				label = "+++";
+				status = "OFFLINE";
+
+				if (cl->typ=='c' || cl->typ=='r' || cl->typ=='p'){
+
+					if (cl->typ == 'c'){
+						type = "user";
+						idx = count_u;
+						label = cl->account->usr;
+						status = "ONLINE";
+						count_u++;
+					}
+
+					else if (cl->typ == 'r'){
+						type = "reader";
+						idx = count_r;
+						label = cl->reader->label;
+						if (cl->reader->card_status == CARD_INSERTED)
+							status = "CONNECTED";
+						count_r++;
+					}
+
+					else if (cl->typ == 'p'){
+						type = "proxy";
+						idx = count_p;
+						label = cl->reader->label;
+						if (cl->reader->card_status == CARD_INSERTED)
+							status = "CARDOK";
+						count_p++;
+					}
+
 					get_servicename(cl, cl->last_srvid, cl->last_caid);
-					fprintf(fpsave,"user%d: %s - %s:%s [%d]\n",
+					fprintf(fpsave,"%s%d: %s - %s:%s [%d] %s\n",
+							type,
 							idx,
-							cl->account->usr,
+							label,
 							cl->last_srvidptr && cl->last_srvidptr->prov ? cl->last_srvidptr->prov : "",
 							cl->last_srvidptr && cl->last_srvidptr->name ? cl->last_srvidptr->name : "",
-							cl->cwlastresptime);
+							cl->cwlastresptime,
+							status);
+
+				}
 
 
-					idx++;
+			}
+
+			time_t now = time((time_t)0);
+			seconds = now - first_client->login;
+			secs = seconds % 60;
+			if (seconds > 60) {
+				fullmins = seconds / 60;
+				mins = fullmins % 60;
+				if(fullmins > 60) {
+					fullhours = fullmins / 60;
+					hours = fullhours % 24;
+					days = fullhours / 24;
 				}
 			}
 
-			fprintf(fpsave,"status0: %s\n", CS_VERSION);
-			fprintf(fpsave,"status1: OSCam Rev. %s\n", CS_SVN_VERSION);
-			//fprintf(fpsave,"status2: %s\n", sec2timeformat(vars, (now - first_client->login)));
-			fprintf(fpsave,"status3: proxies:%d\n", count_p);
-			fprintf(fpsave,"status4: reader:%d\n", count_r);
-			fprintf(fpsave,"status5: user:%d\n", count_u);
+			fprintf(fpsave,"status0: Version: %s\n", CS_VERSION);
+			fprintf(fpsave,"status1: Revision: %s\n", CS_SVN_VERSION);
+			if(days == 0)
+				fprintf(fpsave, "status2: up: %02d:%02d:%02d\n", hours, mins, secs);
+			else
+				fprintf(fpsave, "status2: up: %02dd %02d:%02d:%02d\n", days, hours, mins, secs);
+			fprintf(fpsave,"status3: proxies: %d\n", count_p);
+			fprintf(fpsave,"status4: reader: %d\n", count_r);
+			fprintf(fpsave,"status5: user: %d\n", count_u);
 
 			fclose(fpsave);
 		}
