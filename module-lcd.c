4da1eb083129dcff21839669442f05788aaee20e
alno <alno@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2011-05-26 19:51:09 +0000

    LCD: re-format output of oscam.lcd for direct output to display
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@5300 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-lcd.c
+++ module-lcd.c
@@ -18,7 +18,6 @@ int8_t running;
 
 void refresh_lcd_file() {
 
-
 	char targetfile[256];
 	snprintf(targetfile, sizeof(targetfile),"%s%s", get_tmp_dir(), "/oscam.lcd");
 
@@ -38,123 +37,18 @@ void refresh_lcd_file() {
 			char *label;
 			char *status;
 
-			struct s_client *cl;
-			for ( i=0, cl=first_client; cl ; cl=cl->next, i++) {
-				type = "---";
-				label = "+++";
-				status = "OFFLINE";
-				seconds = 0;
-				secs = 0;
-				fullmins = 0;
-				mins = 0;
-				fullhours = 0;
-				hours = 0;
-				days = 0;
+			// Statuslines start
+			secs = 0; fullmins = 0; mins = 0; fullhours = 0; hours = 0; days = 0;
 
-
-				if (cl->typ=='c' || cl->typ=='r' || cl->typ=='p'){
-
-					seconds = now - cl->lastecm;
-
-					if (cl->typ == 'c' && seconds < 10){
-						type = "user";
-						idx = count_u;
-						label = cl->account->usr;
-						status = "ONLINE";
-						count_u++;
-					}
-
-					else if (cl->typ == 'r'){
-						type = "reader";
-						idx = count_r;
-						label = cl->reader->label;
-						if (cl->reader->card_status == CARD_INSERTED)
-							status = "CARDOK";
-						count_r++;
-					}
-
-					else if (cl->typ == 'p'){
-						type = "proxy";
-						idx = count_p;
-						label = cl->reader->label;
-						if (cl->reader->card_status == CARD_INSERTED)
-							status = "CONNECTED";
-						count_p++;
-					}
-
-					if (cl->typ == 'c'){
-						if(seconds < 10){
-							get_servicename(cl, cl->last_srvid, cl->last_caid);
-							fprintf(fpsave,"%s%d: %-10s %s:%s [%d]\n",
-									type,
-									idx,
-									label,
-									cl->last_srvidptr && cl->last_srvidptr->prov ? cl->last_srvidptr->prov : "",
-									cl->last_srvidptr && cl->last_srvidptr->name ? cl->last_srvidptr->name : "",
-									cl->cwlastresptime);
-						}
-					} else {
-
-
-						secs = seconds % 60;
-						if (seconds > 60) {
-							fullmins = seconds / 60;
-							mins = fullmins % 60;
-							if(fullmins > 60) {
-								fullhours = fullmins / 60;
-								hours = fullhours % 24;
-								days = fullhours / 24;
-							}
-						}
-
-						int16_t written = 0, skipped = 0, blocked = 0, error = 0;
-
-						for (i=0; i<4; i++) {
-							error += cl->reader->emmerror[i];
-							blocked += cl->reader->emmblocked[i];
-							skipped += cl->reader->emmskipped[i];
-							written += cl->reader->emmwritten[i];
-						}
-
-						fprintf(fpsave,"%s%d: %-10s %02d:%02d:%02d  %d/%d/%d/%d %s\n",
-								type,
-								idx,
-								label,
-								hours,
-								mins,
-								secs,
-								written,
-								skipped,
-								blocked,
-								error,
-								status);
-
-					}
-
-				}
-
-
-			}
-
-			seconds = 0;
-			secs = 0;
-			fullmins = 0;
-			mins = 0;
-			fullhours = 0;
-			hours = 0;
-			days = 0;
-
-			if (now > first_client->login){
-				seconds = now - first_client->login;
-				secs = seconds % 60;
-				if (seconds > 60) {
-					fullmins = seconds / 60;
-					mins = fullmins % 60;
-					if(fullmins > 60) {
-						fullhours = fullmins / 60;
-						hours = fullhours % 24;
-						days = fullhours / 24;
-					}
+			seconds = now - first_client->login;
+			secs = seconds % 60;
+			if (seconds > 60) {
+				fullmins = seconds / 60;
+				mins = fullmins % 60;
+				if(fullmins > 60) {
+					fullhours = fullmins / 60;
+					hours = fullhours % 24;
+					days = fullhours / 24;
 				}
 			}
 
@@ -164,14 +58,124 @@ void refresh_lcd_file() {
 				fprintf(fpsave, "status2: up: %02d:%02d:%02d\n", hours, mins, secs);
 			else
 				fprintf(fpsave, "status2: up: %02dd %02d:%02d:%02d\n", days, hours, mins, secs);
-			fprintf(fpsave,"status3: proxies: %d\n", count_p);
-			fprintf(fpsave,"status4: reader: %d\n", count_r);
-			fprintf(fpsave,"status5: user: %d\n", count_u);
-			fprintf(fpsave,"status6: totals: %d/%d/%d/%d/%d/%d\n", first_client->cwfound, first_client->cwnot, first_client->cwignored, first_client->cwtout, first_client->cwcache, first_client->cwtun);
-			fprintf(fpsave,"status7: uptime: %d\n", seconds);
+			fprintf(fpsave,"status3: totals: %d/%d/%d/%d/%d/%d\n", first_client->cwfound, first_client->cwnot, first_client->cwignored, first_client->cwtout, first_client->cwcache, first_client->cwtun);
+			fprintf(fpsave,"status4: uptime: %d\n", seconds);
+			// Statuslines end
 
+			// Readertable head
+			fprintf(fpsave,"Typ | Label      | Idle            | w | s | b | e | Status\n");
+			fprintf(fpsave,"----+------------+-----------------+---+---+---+---+--------\n");
+
+			struct s_client *cl;
+
+			// Reader/Proxy table start
+			for ( i=0, cl=first_client; cl ; cl=cl->next, i++) {
+
+				if (cl->typ=='r' || cl->typ=='p'){
+					type = "";
+					label = "";
+					status = "OFFLINE";
+					secs = 0; fullmins = 0; mins = 0; fullhours = 0; hours = 0; days = 0;
+
+					seconds = now - cl->last;
+
+					if (cl->typ == 'r'){
+						type = "R";
+						idx = count_r;
+						label = cl->reader->label;
+						if (cl->reader->card_status == CARD_INSERTED)
+							status = "CARDOK";
+						count_r++;
+					}
+
+					else if (cl->typ == 'p'){
+						type = "P";
+						idx = count_p;
+						label = cl->reader->label;
+						if (cl->reader->card_status == CARD_INSERTED)
+							status = "CONNECTED";
+						count_p++;
+					}
+
+
+					secs = seconds % 60;
+					if (seconds > 60) {
+						fullmins = seconds / 60;
+						mins = fullmins % 60;
+						if(fullmins > 60) {
+							fullhours = fullmins / 60;
+							hours = fullhours % 24;
+							days = fullhours / 24;
+						}
+					}
+
+					int16_t written = 0, skipped = 0, blocked = 0, error = 0;
+
+					for (i=0; i<4; i++) {
+						error += cl->reader->emmerror[i];
+						blocked += cl->reader->emmblocked[i];
+						skipped += cl->reader->emmskipped[i];
+						written += cl->reader->emmwritten[i];
+					}
+
+					if(days == 0) {
+						fprintf(fpsave,"%s%d  | %-10.10s |        %02d:%02d:%02d |% 3d|% 3d|% 3d|% 3d| %s\n",
+								type, idx, label, hours, mins,
+								secs, written, skipped, blocked, error, status);
+					} else {
+						fprintf(fpsave,"%s%d  | %-10.10s |% 5dd %02d:%02d:%02d |% 3d|% 3d|% 3d|% 3d| %s\n",
+								type, idx, label, days, hours, mins,
+								secs, written, skipped, blocked, error, status);
+					}
+
+				}
+			}
+
+			fprintf(fpsave,"----+------------+-----------------+---+---+---+---+--------\n");
+			// Reader/Proxy table end
+
+
+			// Usertable start
+			fprintf(fpsave,"Typ | Label      | Provider : Channel              | Time\n");
+			fprintf(fpsave,"----+------------+---------------------------------+--------\n");
+
+			/* Testclient
+			fprintf(fpsave,"%s%d  | %-10.10s | %-10.10s:%-20.20s | % 4dms\n",
+					"U",
+					1,
+					"test",
+					"Sky De",
+					"Discovery Channel",
+					568);
+
+			*/
+
+			for ( i=0, cl=first_client; cl ; cl=cl->next, i++) {
+
+				seconds = now - cl->lastecm;
+
+				if (cl->typ == 'c' && seconds < 15){
+					type = "U";
+					idx = count_u;
+					label = cl->account->usr;
+					count_u++;
+
+					get_servicename(cl, cl->last_srvid, cl->last_caid);
+					fprintf(fpsave,"%s%d  | %-10.10s | %-10.10s:%-20.20s | % 4dms\n",
+							type,
+							idx,
+							label,
+							cl->last_srvidptr && cl->last_srvidptr->prov ? cl->last_srvidptr->prov : "",
+									cl->last_srvidptr && cl->last_srvidptr->name ? cl->last_srvidptr->name : "",
+											cl->cwlastresptime);
+
+				}
+			}
+			fprintf(fpsave,"----+------------+---------------------------------+--------\n");
+			// Usertable end
 			fclose(fpsave);
 		}
+
 		idx = 0;
 		cs_sleepms(10000);
 		cnt++;
