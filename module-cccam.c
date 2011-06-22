b1175e174813021796f2ac7014999fe486d63d81
schlocke <schlocke@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2011-06-22 20:52:11 +0000

    cccam: fixed loosing cards. Now cards are removed before added. Before, it was add, then remove
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@5466 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-cccam.c
+++ module-cccam.c
@@ -1906,7 +1906,7 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l) {
 			it = ll_iter_create(cc->cards);
 			struct cc_card *old_card;
 			while ((old_card = ll_iter_next(&it))) {
-				if (old_card->id == card->id) //we aready have this card, delete it
+				if (old_card->id == card->id || same_card(old_card, card)) //we aready have this card, delete it
 				{
 					cc_free_card(card);
 					card = old_card;
