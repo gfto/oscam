0b38808280213749cf538e32584b7fe5e7f14997
lareq78 <lareq78@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2011-07-08 11:03:32 +0000

    try quick fix for newcamd connection close
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@5570 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-newcamd.c
+++ module-newcamd.c
@@ -360,7 +360,7 @@ static int32_t connect_newcamd_server()
   cl->reader->last_g = cl->reader->last_s = time((time_t *)0);
 
   // Only after connect() on cl->udp_fd (Linux)
-  cl->pfd=cl->udp_fd;     
+  // cl->pfd=cl->udp_fd;     
 
   return 0;
 }
@@ -1038,10 +1038,8 @@ static void * newcamd_server(void *cli)
   rc=-9;
   while(rc==-9)
   {
-    if (!client->pfd) {
-    cs_log("duplicate newcamd users!");
-    break;
-    }
+    if (!client->pfd)   break;
+   
     // process_input returns -9 on clienttimeout
     while ((rc=process_input(mbuf, sizeof(mbuf), cfg.cmaxidle))>0)
     {
