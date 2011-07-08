998466474d26b2aba875f7dae0bcb2a3c236ec1e
alno <alno@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2011-07-08 18:45:56 +0000

    Config: fix invalid errormessage
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@5572 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam-config.c
+++ oscam-config.c
@@ -4268,6 +4268,8 @@ void chk_reader(char *token, char *value, struct s_reader *rdr)
 			rdr->blockemmbylen[i] = 0;
 		for (i = 0, ptr = strtok_r(value, ",", &saveptr1); (i < CS_MAXEMMBLOCKBYLEN) && (ptr); ptr = strtok_r(NULL, ",", &saveptr1), i++)
 			rdr->blockemmbylen[i] = atoi(ptr);
+
+		return;
 	}
 
 #ifdef WITH_LB
