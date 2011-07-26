5de29d83a0cfea5443dbc96940683bc2ecde250f
alno <alno@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2011-07-26 16:58:11 +0000

    WebIf: add ATR to entitlements if reader is pcsc
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@5732 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- csctapi/ifd_pcsc.c
+++ csctapi/ifd_pcsc.c
@@ -172,6 +172,8 @@ int32_t pcsc_activate_card(struct s_reader *pcsc_reader, uchar *atr, uint16_t *a
         pcsc_reader->init_history_pos=0;
 
         cs_log("%s ATR: %s",pcsc_reader->label, cs_hexdump(1, (uchar *)pbAtr, dwAtrLen, tmp, sizeof(tmp)));
+    	memcpy(reader->card_atr, pbAtr, dwAtrLen);
+    	reader->card_atr_length = dwAtrLen;
         return(1);
     }
     else {
