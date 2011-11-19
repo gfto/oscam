35d93ad45f4a613405c7ab1ce0d2e899065a8827
corsair <corsair@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2011-11-19 12:43:08 +0000

    fixed nagra entitlements (added return ut)
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@6031 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- reader-nagra.c
+++ reader-nagra.c
@@ -29,6 +29,7 @@ static time_t tier_date(uint32_t date, char *buf, int32_t l)
 	gmtime_r(&ut, &t);
 	snprintf(buf, l, "%04d/%02d/%02d", t.tm_year+1900, t.tm_mon+1, t.tm_mday);
     }
+  return ut;
 }
 
 static char *nagra_datetime(struct s_reader *rdr, uint8_t *ndays, int32_t offset, char *result, time_t *t)
