5180fac139bf0197a3a6cd4197d835e2c6ca0ee2
corsair <corsair@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2011-12-25 18:18:44 +0000

    fixed cacheex=3 with camd35
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@6140 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-camd35.c
+++ module-camd35.c
@@ -376,11 +376,15 @@ static int32_t tcp_connect()
 #ifdef CS_CACHEEX
 int32_t camd35_cache_push_out(struct s_client *cl, struct ecm_request_t *er)
 {
-	if (!cl->udp_fd) return(-1);
-	if (cl->reader)
-		cl->reader->last_s = cl->reader->last_g = time((time_t *)0);
 	int8_t rc = (er->rc<E_NOTFOUND)?E_FOUND:er->rc;
 	if (rc != E_FOUND) return -1; //Maybe later we could support other rcs
+
+	if (cl->reader) {
+		tcp_connect();
+		cl->reader->last_s = cl->reader->last_g = time((time_t *)0);
+	}
+	if (!cl->udp_fd) return(-1);
+
 	unsigned char *buf = cs_malloc(&buf, 20 + er->l + 16 + 64, 0);
 
 	memset(buf, 0, 20);
@@ -407,10 +411,11 @@ int32_t camd35_cache_push_out(struct s_client *cl, struct ecm_request_t *er)
 
 void camd35_cache_push_in(struct s_client *cl, uchar *buf)
 {
-	if (cl->reader)
-		cl->reader->last_s = cl->reader->last_g = time((time_t *)0);
 	if (buf[3] >= E_NOTFOUND) //Maybe later we could support other rcs
 		return;
+
+	if (cl->reader)
+		cl->reader->last_s = cl->reader->last_g = time((time_t *)0);
 
 	ECM_REQUEST *er;
 	int16_t testlen = buf[1] | (buf[2] << 8);
