eef81b99054bcc4942749f2be2d7a4dff1cc0b11
Admin <Admin@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2012-01-08 21:18:56 +0000

    Happy new Year ;)
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@6212 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam.c
+++ oscam.c
@@ -215,7 +215,7 @@ static const char *logo = "  ___  ____   ___                \n / _ \\/ ___| / __
 static void usage()
 {
   fprintf(stderr, "%s\n\n", logo);
-  fprintf(stderr, "OSCam cardserver v%s, build #%s (%s) - (w) 2009-2011 Streamboard SVN\n", CS_VERSION_X, CS_SVN_VERSION, CS_OSTYPE);
+  fprintf(stderr, "OSCam cardserver v%s, build #%s (%s) - (w) 2009-2012 Streamboard SVN\n", CS_VERSION_X, CS_SVN_VERSION, CS_OSTYPE);
   fprintf(stderr, "\tsee http://streamboard.gmc.to/oscam/ for more details\n");
   fprintf(stderr, "\tbased on Streamboard mp-cardserver v0.9d - (w) 2004-2007 by dukat\n");
   fprintf(stderr, "\tThis program is distributed under GPL.\n");
