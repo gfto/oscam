b17b296a2dd3203d3cc1ba019751ff4a05ef7f5e
Admin <Admin@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2012-02-04 00:12:34 +0000

    Fix stapi compile without cardreader.
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@6339 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- csctapi/atr.c
+++ csctapi/atr.c
@@ -48,7 +48,7 @@ const uint32_t atr_i_table[4] = {25, 50, 100, 0};
 /* 
  * Exported funcions definition
  */
-#if defined(WITH_CARDREADER) || defined(WITH_STAPI)
+#ifdef WITH_CARDREADER
 int32_t ATR_InitFromArray (ATR * atr, const BYTE atr_buffer[ATR_MAX_SIZE], uint32_t length)
 {
 	BYTE TDi;
@@ -198,8 +198,7 @@ int32_t ATR_InitFromArray (ATR * atr, const BYTE atr_buffer[ATR_MAX_SIZE], uint3
     }
 	return (ATR_OK);
 }
-#endif
-#ifdef WITH_CARDREADER
+
 int32_t ATR_GetConvention (ATR * atr, int32_t *convention)
 {
 	if (atr->TS == 0x3B)
