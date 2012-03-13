d9850372ae6d1e3f461b21ceef9dbf323e91fbda
theparasol <theparasol@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2012-03-13 05:48:35 +0000

    Tryfix2 for caiway card -> please test!
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@6506 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- reader-irdeto.c
+++ reader-irdeto.c
@@ -168,7 +168,7 @@ static time_t chid_date(struct s_reader * reader, uint32_t date, char *buf, int3
 							   {0x0604, 0x0005, "NLD", 1066089600L},   // Ziggo irdeto card caid: 0604, acs: 0.05
 							   {0x0602, 0x0606, "NLD", 76208400L},     // Ziggo (before Casema) irdeto card gray-blue caid: 0602, acs: 6.06
 							   {0x0602, 0x0505, "NLD", 946602000L},    // Ziggo (before Casema) irdeto card black-orange caid: 0602, acs: 5.05
-							   {0x0606, 0x0605, "NLD", 76208400L},     // Caiway irdeto card caid: 0606, acs: 6.05  *** UNTESTED, PLEASE TEST ***
+							   {0x0606, 0x0605, "NLD", 461026800L},     // Caiway irdeto card caid: 0606, acs: 6.05  *** UNTESTED, PLEASE TEST ***
 
 
                                // {0x1702, 0x0384, "AUT", XXXXXXXXXL},     // -> we need the base date for this
