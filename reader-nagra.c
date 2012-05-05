24a9727cb01c74b94f7c8cde94088b5ae640e6b1
gf <gf@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2012-05-05 09:57:23 +0000

    reader-nagra: Remove unneeded ifdef checks for WITH_LIBCRYPTO.
    cscrypt supports BN_CTX_start() just fine, so it makes no sense
    to issue BN_CTX_start() only when OSCAM is compiled with libcrypto.
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@6672 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- reader-nagra.c
+++ reader-nagra.c
@@ -187,9 +187,7 @@ static int32_t NegotiateSessionKey_Tiger(struct s_reader * reader)
 	}
 	
 	BN_CTX *ctx = BN_CTX_new();
-#ifdef WITH_LIBCRYPTO
 	BN_CTX_start(ctx);
-#endif
 	BIGNUM *bnN = BN_CTX_get(ctx);
 	BIGNUM *bnE = BN_CTX_get(ctx);
 	BIGNUM *bnCT = BN_CTX_get(ctx);
@@ -227,9 +225,7 @@ static int32_t NegotiateSessionKey_Tiger(struct s_reader * reader)
 	
 	ReverseMem(cta_res+2, 88);
 	BN_CTX *ctx1 = BN_CTX_new();
-#ifdef WITH_LIBCRYPTO
 	BN_CTX_start(ctx1);
-#endif
 	BIGNUM *bnN1 = BN_CTX_get(ctx1);
 	BIGNUM *bnE1 = BN_CTX_get(ctx1);
 	BIGNUM *bnCT1 = BN_CTX_get(ctx1);
@@ -264,9 +260,7 @@ static int32_t NegotiateSessionKey_Tiger(struct s_reader * reader)
 	
 	
 	BN_CTX *ctx3 = BN_CTX_new();
-#ifdef WITH_LIBCRYPTO
 	BN_CTX_start(ctx3);
-#endif
 	BIGNUM *bnN3 = BN_CTX_get(ctx3);
 	BIGNUM *bnE3 = BN_CTX_get(ctx3);
 	BIGNUM *bnCT3 = BN_CTX_get(ctx3);
@@ -359,9 +353,7 @@ static int32_t NegotiateSessionKey(struct s_reader * reader)
 	ReverseMem(cta_res+2, 64);
 	unsigned char vFixed[] = {0,1,2,3};
 	BN_CTX *ctx = BN_CTX_new();
-#ifdef WITH_LIBCRYPTO
 	BN_CTX_start(ctx);
-#endif
 	BIGNUM *bnN = BN_CTX_get(ctx);
 	BIGNUM *bnE = BN_CTX_get(ctx);
 	BIGNUM *bnCT = BN_CTX_get(ctx);
