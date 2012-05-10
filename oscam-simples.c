5d25ec8894fe1bdab1f9b9d7e739b6d32837fa6d
gf <gf@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2012-05-10 12:43:06 +0000

    Fix warning seen on OSX build.
    The warning is related to using possibly uninitalized variable.
    The code is actually correct but the compiler does not see it.
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@6760 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam-simples.c
+++ oscam-simples.c
@@ -1429,7 +1429,7 @@ int32_t ecmfmt(uint16_t caid, uint32_t prid, uint16_t chid, uint16_t pid, uint16
 	if (!cfg.ecmfmt[0])
 		return snprintf(result, size, "%04X&%06X/%04X/%04X/%02X:%04X", caid, prid, chid, srvid, l, htons(checksum));
 
-	uint32_t s=0, zero=0, flen, value=0;
+	uint32_t s=0, zero=0, flen=0, value=0;
 	char *c = cfg.ecmfmt, fmt[5] = "%04X";
 	while (*c) {
 		switch(*c)
@@ -1444,10 +1444,9 @@ int32_t ecmfmt(uint16_t caid, uint32_t prid, uint16_t chid, uint16_t pid, uint16
 			case 'h': flen=4; value=htons(checksum); break;
 			case '\\':
 				c++;
-				flen=0;
 				value=*c;
 				break;
-			default:  flen=0; value=*c; break;
+			default: value=*c; break;
 		}
 		if (value) zero=0;
 
