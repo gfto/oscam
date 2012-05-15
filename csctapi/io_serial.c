4052aa4c26c6565b29a822fd9b729c275ee2f424
theparasol <theparasol@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2012-05-15 17:07:03 +0000

    Tryfix 3 VU Box internal reader
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@6832 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- csctapi/io_serial.c
+++ csctapi/io_serial.c
@@ -451,8 +451,8 @@ bool IO_Serial_Read (struct s_reader * reader, uint32_t timeout, uint32_t size, 
 		{
 			while (readed == 0){
 				readed = read (reader->handle, &c, 1);
-				if (readed < 0) {
-					cs_log("Reader %s: ERROR in IO_Serial_Read (errno=%d %s)", reader->label, errno, strerror(errno));
+				if (readed < 1) {
+					if (readed !=0) cs_log("Reader %s: ERROR in IO_Serial_Read (errno=%d %s)", reader->label, errno, strerror(errno));
 					if (errorcount > 10) return ERROR;
 					errorcount++;
 					tcflush (reader->handle, TCIFLUSH);
@@ -493,9 +493,10 @@ bool IO_Serial_Write (struct s_reader * reader, uint32_t delay, uint32_t size, c
 			while (to_do !=0){
 				cs_ddump_mask(D_DEVICE, data_w, to_send, "IO: Sending: ");
 				int32_t u = write (reader->handle, data_w, to_send);
-				if (u < 0) {
+				if (u < 1) {
 					errorcount++;
-					cs_log("Reader %s: ERROR in IO_Serial_Write actual written=%d of=%d (errno=%d %s)", reader->label, (size - to_do), size, errno, strerror(errno));
+					tcflush (reader->handle, TCIFLUSH);
+					if (u != 0) cs_log("Reader %s: ERROR in IO_Serial_Write actual written=%d of=%d (errno=%d %s)", reader->label, (size - to_do), size, errno, strerror(errno));
 					if (errorcount > 10) return ERROR; //exit if more than 10 errors
 					}
 				else {
@@ -503,7 +504,6 @@ bool IO_Serial_Write (struct s_reader * reader, uint32_t delay, uint32_t size, c
 					errorcount = 0;
 					if ((reader->typ != R_INTERNAL && reader->crdr.active==0) || (reader->crdr.active==1 && reader->crdr.read_written==1))
 					reader->written += u;
-					tcflush (reader->handle, TCIFLUSH);
 					}
 			}
 		}
