21e772a8b66995346f4876c66b38dba162ada9ed
gf <gf@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2012-05-28 10:15:48 +0000

    csctapi/ifd_cool: Add cnxt_smc_XXXX() declarations.
    These functions are in libnxp and were missing proper prototypes.
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@6935 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- csctapi/ifd_cool.h
+++ csctapi/ifd_cool.h
@@ -2,9 +2,21 @@
     ifd_cool.h
     Header file for Coolstream internal reader.
 */
-#ifdef COOL
+#ifndef CSCTAPI_IFD_COOL_H_
+#define CSCTAPI_IFD_COOL_H_
 
 #include "atr.h"
+
+int32_t cnxt_smc_open(void *cool_handle, int32_t *, void *, void *);
+int32_t cnxt_smc_enable_flow_control(void *cool_handle);
+int32_t cnxt_smc_get_state(void *cool_handle, int32_t *state);
+int32_t cnxt_smc_get_clock_freq(void *cool_handle, uint32_t *clk);
+int32_t cnxt_smc_reset_card(void *cool_handle, int timeout, void *, void *);
+int32_t cnxt_smc_get_atr(void *cool_handle, unsigned char *buf, int32_t *buflen);
+int32_t cnxt_smc_read_write(void *cool_handle, int32_t b, uint8_t *sent, uint32_t size, char *cardbuffer, uint32_t *cardbuflen, int32_t rw_timeout, int);
+int32_t cnxt_smc_set_clock_freq(void *cool_handle, int32_t clk);
+int32_t cnxt_smc_close(void *cool_handle);
+
 int32_t Cool_Init (struct s_reader *reader);
 int32_t Cool_Reset (struct s_reader *reader, ATR * atr);
 int32_t Cool_Transmit (struct s_reader *reader, BYTE * buffer, uint32_t size);
@@ -16,5 +28,5 @@ int32_t Cool_FastReset_With_ATR (struct s_reader *reader, ATR * atr);
 int32_t Cool_GetStatus (struct s_reader *reader, int32_t * in);
 int32_t Cool_WriteSettings (struct s_reader *reader, uint32_t BWT, uint32_t CWT, uint32_t EGT, uint32_t BGT);
 int32_t Cool_Close (struct s_reader *reader);
-void * handle;
+
 #endif
