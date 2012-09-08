4fbbf7cb14f51ddc9d7b5d8179d1de473efe590a
gf <gf@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2012-09-08 21:25:14 +0000

    cfg: Move basic config parsing code to oscam-conf.{c,h}
    Just code movement, no code changes.
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@7379 4b0bc96b-bc66-0410-9d44-ebda105a78c1

---
+++ oscam-conf.c
@@ -0,0 +1,137 @@
+#include "globals.h"
+#include "oscam-conf.h"
+
+#define CONFVARWIDTH 30
+
+/* Returns the default value if string length is zero, otherwise atoi is called*/
+int32_t strToIntVal(char *value, int32_t defaultvalue){
+	if (strlen(value) == 0) return defaultvalue;
+	errno = 0; // errno should be set to 0 before calling strtol
+	int32_t i = strtol(value, NULL, 10);
+	return (errno == 0) ? i : defaultvalue;
+}
+
+/* Returns the default value if string length is zero, otherwise strtoul is called*/
+uint32_t strToUIntVal(char *value, uint32_t defaultvalue){
+	if (strlen(value) == 0) return defaultvalue;
+	errno = 0; // errno should be set to 0 before calling strtoul
+	uint32_t i = strtoul(value, NULL, 10);
+	return (errno == 0) ? i : defaultvalue;
+}
+
+ /* Replacement of fprintf which adds necessary whitespace to fill up the varname to a fixed width.
+   If varname is longer than CONFVARWIDTH, no whitespace is added*/
+void fprintf_conf(FILE *f, const char *varname, const char *fmtstring, ...){
+	int32_t varlen = strlen(varname);
+	int32_t max = (varlen > CONFVARWIDTH) ? varlen : CONFVARWIDTH;
+	char varnamebuf[max + 3];
+	char *ptr = varnamebuf + varlen;
+	va_list argptr;
+
+	cs_strncpy(varnamebuf, varname, sizeof(varnamebuf));
+	while(varlen < CONFVARWIDTH){
+		ptr[0] = ' ';
+		++ptr;
+		++varlen;
+	}
+	cs_strncpy(ptr, "= ", sizeof(varnamebuf)-(ptr-varnamebuf));
+	if (fwrite(varnamebuf, sizeof(char), strlen(varnamebuf), f)){
+		if(strlen(fmtstring) > 0){
+			va_start(argptr, fmtstring);
+			vfprintf(f, fmtstring, argptr);
+			va_end(argptr);
+		}
+	}
+}
+
+int config_list_parse(const struct config_list *clist, const char *token, char *value, void *config_data) {
+	const struct config_list *c;
+	for (c = clist; c->opt_type != OPT_UNKNOWN; c++) {
+		if (strcasecmp(token, c->config_name) != 0)
+			continue;
+		void *cfg = config_data + c->var_offset;
+		switch (c->opt_type) {
+		case OPT_INT: {
+			*(int32_t *)cfg = strToIntVal(value, c->default_value);
+			return 1;
+		}
+		case OPT_UINT: {
+			*(uint32_t *)cfg = strToUIntVal(value, c->default_value);
+			return 1;
+		}
+		case OPT_STRING: {
+			char **scfg = cfg;
+			NULLFREE(*scfg);
+			if (strlen(value))
+				*scfg = strdup(value);
+			return 1;
+		}
+		case OPT_SSTRING: {
+			char *scfg = cfg;
+			scfg[0] = '\0';
+			unsigned int len = strlen(value);
+			if (len) {
+				strncpy(scfg, value, c->str_size - 1);
+				if (len > c->str_size) {
+					fprintf(stderr, "WARNING: Config value for '%s' (%s, len=%d) exceeds max length: %d (%s)\n",
+						token, value, len, c->str_size - 1, scfg);
+				}
+			}
+			return 1;
+		}
+		case OPT_FUNC: {
+			if (c->process_fn)
+				c->process_fn(token, value, cfg, NULL);
+			return 1;
+		}
+		case OPT_UNKNOWN: {
+			fprintf(stderr, "Unknown config type (%s = %s).", token, value);
+			break;
+		}
+		}
+	}
+	return 0;
+}
+
+void config_list_save(FILE *f, const struct config_list *clist, void *config_data, int save_all) {
+	const struct config_list *c;
+	for (c = clist; c->opt_type != OPT_UNKNOWN; c++) {
+		void *cfg = config_data + c->var_offset;
+		switch (c->opt_type) {
+		case OPT_INT: {
+			int32_t val = *(int32_t *)cfg;
+			if (save_all || val != c->default_value)
+				fprintf_conf(f, c->config_name, "%d\n", val);
+			continue;
+		}
+		case OPT_UINT: {
+			uint32_t val = *(uint32_t *)cfg;
+			if (save_all || val != (uint32_t)c->default_value)
+				fprintf_conf(f, c->config_name, "%u\n", val);
+			continue;
+		}
+		case OPT_STRING: {
+			char **val = cfg;
+			if (*val || save_all) {
+				if (*val && strlen(*val))
+					fprintf_conf(f, c->config_name, "%s\n", *val);
+			}
+			continue;
+		}
+		case OPT_SSTRING: {
+			char *val = cfg;
+			if (val[0] || save_all) {
+				fprintf_conf(f, c->config_name, "%s\n", val);
+			}
+			continue;
+		}
+		case OPT_FUNC: {
+			if (c->process_fn)
+				c->process_fn((const char *)c->config_name, NULL, cfg, f);
+			continue;
+		}
+		case OPT_UNKNOWN:
+			break;
+		}
+	}
+}
---
+++ oscam-conf.h
@@ -0,0 +1,77 @@
+#ifndef OSCAM_CONF_H
+#define OSCAM_CONF_H
+
+#include <stdio.h>
+#include <inttypes.h>
+
+enum opt_types {
+	OPT_UNKNOWN = 0,
+	OPT_INT     = 1 << 1,
+	OPT_UINT    = 1 << 2,
+	OPT_STRING  = 1 << 3,
+	OPT_SSTRING = 1 << 4,
+	OPT_FUNC    = 1 << 5,
+};
+
+struct config_list {
+	enum opt_types	opt_type;
+	char			*config_name;
+	size_t			var_offset;
+	long			default_value;
+	unsigned int	str_size;
+	void			(*process_fn)(const char *token, char *value, void *setting, FILE *config_file);
+};
+
+#define DEF_OPT_INT(__name, __var_ofs, __default) \
+	{ \
+		.opt_type		= OPT_INT, \
+		.config_name	= __name, \
+		.var_offset		= __var_ofs, \
+		.default_value	= __default \
+	}
+
+#define DEF_OPT_UINT(__name, __var_ofs, __default) \
+	{ \
+		.opt_type		= OPT_UINT, \
+		.config_name	= __name, \
+		.var_offset		= __var_ofs, \
+		.default_value	= __default \
+	}
+
+#define DEF_OPT_STR(__name, __var_ofs) \
+	{ \
+		.opt_type		= OPT_STRING, \
+		.config_name	= __name, \
+		.var_offset		= __var_ofs \
+	}
+
+#define DEF_OPT_SSTR(__name, __var_ofs, __str_size) \
+	{ \
+		.opt_type		= OPT_SSTRING, \
+		.config_name	= __name, \
+		.var_offset		= __var_ofs, \
+		.str_size		= __str_size \
+	}
+
+#define DEF_OPT_FUNC(__name, __var_ofs, __process_fn) \
+	{ \
+		.opt_type		= OPT_FUNC, \
+		.config_name	= __name, \
+		.var_offset		= __var_ofs, \
+		.process_fn		= __process_fn \
+	}
+
+#define DEF_LAST_OPT \
+	{ \
+		.opt_type		= OPT_UNKNOWN \
+	}
+
+int32_t  strToIntVal(char *value, int32_t defaultvalue);
+uint32_t strToUIntVal(char *value, uint32_t defaultvalue);
+
+void fprintf_conf(FILE *f, const char *varname, const char *fmt, ...);
+
+int  config_list_parse(const struct config_list *clist, const char *token, char *value, void *config_data);
+void config_list_save(FILE *f, const struct config_list *clist, void *config_data, int save_all);
+
+#endif
