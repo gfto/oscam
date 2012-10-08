97cedc4d6f352365df692c859706c4628afb4507
gf <gf@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2012-10-08 15:07:17 +0000

    Add missing oscam-log.h file.
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@7649 4b0bc96b-bc66-0410-9d44-ebda105a78c1

---
+++ oscam-log.h
@@ -0,0 +1,29 @@
+#ifndef OSCAM_LOG_H_
+#define OSCAM_LOG_H_
+
+int32_t cs_init_log(void);
+int32_t cs_open_logfiles(void);
+void cs_log_config(void);
+void cs_close_log(void);
+
+void cs_disable_log(int8_t disabled);
+
+void cs_reinit_loghist(uint32_t size);
+
+void cs_log_int(uint16_t mask, int8_t lock, const uchar *buf, int32_t n, const char *fmt, ...) __attribute__ ((format (printf, 5, 6)));
+
+#define cs_log(...)          cs_log_int(0, 1, NULL, 0, ##__VA_ARGS__)
+#define cs_log_nolock(...)   cs_log_int(0, 0, NULL, 0, ##__VA_ARGS__)
+#define cs_dump(buf, n, ...) cs_log_int(0, 1, buf,  n, ##__VA_ARGS__)
+
+#define cs_debug_mask(mask, ...)         do { if (config_WITH_DEBUG()) cs_log_int(mask, 1, NULL, 0, ##__VA_ARGS__); } while(0)
+#define cs_debug_mask_nolock(mask, ...)  do { if (config_WITH_DEBUG()) cs_log_int(mask, 0, NULL, 0, ##__VA_ARGS__); } while(0)
+#define cs_ddump_mask(mask, buf, n, ...) do { if (config_WITH_DEBUG()) cs_log_int(mask, 1, buf , n, ##__VA_ARGS__); } while(0)
+
+void log_emm_request(struct s_reader *rdr);
+void logCWtoFile(ECM_REQUEST *er, uchar *cw);
+
+int32_t cs_init_statistics(void);
+void cs_statistics(struct s_client * client);
+
+#endif
