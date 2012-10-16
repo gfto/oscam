d5ac07d03f296b5f7ff43e14c83acf3c9202c9f1
theparasol <theparasol@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2012-10-16 22:09:59 +0000

    Softfail malformed card should be initialized also
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@7740 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- csctapi/ifd_pcsc.c
+++ csctapi/ifd_pcsc.c
@@ -210,7 +210,7 @@ static int32_t pcsc_activate(struct s_reader *reader, struct s_ATR *atr)
     unsigned char atrarr[ATR_MAX_SIZE];
     uint16_t atr_size = 0;
     if (pcsc_activate_card(reader, atrarr, &atr_size) == OK) {
-        if (ATR_InitFromArray(atr, atrarr, atr_size) == ATR_OK)
+        if (ATR_InitFromArray(atr, atrarr, atr_size) != ERROR) // ATR is OK or softfail malformed 
             return OK;
         else
             return ERROR;
