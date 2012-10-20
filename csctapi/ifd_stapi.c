8e209b9530ade10e3465516f5f42b1ba4ccc00ea
gf <gf@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2012-10-20 12:52:11 +0000

    csctapi: Remove ifd_stapi.h
    This file contains no public functions, all of the functions are
    implemented in liboscam_stapi and are used only in ifd_stapi.c
    
    No binary changes.
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@7762 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- csctapi/ifd_stapi.c
+++ csctapi/ifd_stapi.c
@@ -2,7 +2,16 @@
 
 #if defined(WITH_CARDREADER) && defined(WITH_STAPI)
 #include "atr.h"
-#include "ifd_stapi.h"
+
+/* These functions are implemented in liboscam_stapi.a */
+extern int32_t STReader_Open(char *device, uint32_t *stsmart_handle);
+extern int32_t STReader_GetStatus(uint32_t stsmart_handle, int32_t *in);
+extern int32_t STReader_Reset(uint32_t stsmart_handle, ATR *atr);
+extern int32_t STReader_Transmit(uint32_t stsmart_handle, unsigned char *sent, uint32_t size);
+extern int32_t STReader_Receive(uint32_t stsmart_handle, unsigned char *data, uint32_t size);
+extern int32_t STReader_Close(uint32_t stsmart_handle);
+extern int32_t STReader_SetProtocol(uint32_t stsmart_handle, unsigned char *params, unsigned *length, uint32_t len_request);
+extern int32_t STReader_SetClockrate(uint32_t stsmart_handle);
 
 static int32_t stapi_init(struct s_reader *reader) {
 	return STReader_Open(reader->device, &reader->stsmart_handle);
