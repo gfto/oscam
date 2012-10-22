1bef6528fd0e09a17843be58ed871738cc40eaf1
Deas <Deas@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2012-10-22 21:12:20 +0000

    reader-irdeto.c - show correct date on new orf ice card. see: http://www.streamboard.tv/wbb2/thread.php?postid=429838#post429838
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@7790 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- reader-irdeto.c
+++ reader-irdeto.c
@@ -159,6 +159,7 @@ static time_t chid_date(struct s_reader * reader, uint32_t date, char *buf, int3
                                {0x0666, 0x0006, "SVK", 946598400L},    // 30.12.1999, 16:00    //cslink irdeto
                                {0x0666, 0x0006, "CZE", 946598400L},    // 30.12.1999, 16:00    //cslink irdeto
                                {0x0648, 0x0608, "AUT", 946598400L},    // 31.12.1999, 00:00    //orf ice irdeto
+							   {0x0648, 0x0005, "AUT", 946598400L},    // 31.12.1999, 00:00    //orf ice irdeto
                                {0x0604, 0x0605, "GRC", 1011052800L},   // 15.01.2002, 00:00    //nova irdeto
                                {0x0604, 0x0606, "GRC", 1011052800L},   // 15.01.2002, 00:00    //nova irdeto
                                {0x0604, 0x0607, "GRC", 1011052800L},   // 15.01.2002, 00:00    //nova irdeto
