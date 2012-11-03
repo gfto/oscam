282aecf865d4d9edae9d44a723aa40a7cd8f12f7
theparasol <theparasol@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2012-11-03 21:01:35 +0000

    Where is my head... also forgot the hex notation :( -> fixed!
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@7855 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- reader-videoguard2.c
+++ reader-videoguard2.c
@@ -200,7 +200,7 @@ static void do_post_dw_hash(struct s_reader *reader, unsigned char *cw, const un
         {
           memset(buffer, 0, sizeof(buffer));
           memcpy(buffer, cw, 8);
-		  if ((ecm_header_data[ecmi] - 2) >0 && (ecm_header_data[ecmi] - 2) <=72){
+		  if ((ecm_header_data[ecmi] - 2) >0x00 && (ecm_header_data[ecmi] - 2) <=0x78){
           memcpy(buffer + 8, &ecm_header_data[ecmi + 3], ecm_header_data[ecmi] - 2);
           MD5(buffer, 8 + ecm_header_data[ecmi] - 2, md5tmp);
           memcpy(cw, md5tmp, 8);
