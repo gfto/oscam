761cd157c0761aa6257c61279ee49428845899e7
theparasol <theparasol@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2012-11-13 18:56:37 +0000

    Tryfix for ticket 2946
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@7903 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- csctapi/io_serial.c
+++ csctapi/io_serial.c
@@ -325,6 +325,7 @@ bool IO_Serial_SetProperties (struct s_reader * reader, struct termios newtio)
 	int32_t mctl; 
 	rdr_debug_mask(reader, D_DEVICE, "Getting readerstatus..."); 
 	if (ioctl (reader->handle, TIOCMGET, &mctl) >= 0) {  // get reader statusbits 
+		mctl &= ~TIOCM_RTS;
 		rdr_debug_mask(reader, D_DEVICE, "Set reader ready to Send"); 
 		ioctl (reader->handle, TIOCMSET, &mctl);  // set reader ready to send.
 	} 
