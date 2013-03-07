edb21b2cfd54c76352219eb5d3a49f401583ba7d
gf <gf@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2013-03-07 01:13:21 +0000

    Remove pcsc variables in struct s_config when the reader is not enabled.
    Saves ~128 bytes in configurations without PCSC.
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@8502 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- globals.h
+++ globals.h
@@ -1154,8 +1154,6 @@ struct s_reader  									//contains device info, reader info and card info
 #ifdef WITH_STAPI
 	uint32_t		stsmart_handle;					// device handle for stsmart driver
 #endif
-	char			pcsc_name[128];
-	int8_t			pcsc_has_card;
 	int32_t			detect;
 	int32_t			mhz;							// actual clock rate of reader in 10khz steps
 	int32_t			cardmhz;						// standard clock speed your card should have in 10khz steps; normally 357 but for Irdeto cards 600
@@ -1246,6 +1244,8 @@ struct s_reader  									//contains device info, reader info and card info
 	int32_t			lbvalue;						// loadbalance Value
 #endif
 #ifdef CARDREADER_PCSC
+	char			pcsc_name[128];
+	int8_t			pcsc_has_card;
 	SCARDCONTEXT	hContext;
 	SCARDHANDLE		hCard;
 	DWORD			dwActiveProtocol;
