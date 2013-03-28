b62bdf949e6dc0a25a2f0c15c96e369cc57c57c8
theparasol <theparasol@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2013-03-28 19:59:38 +0000

    Tryfix2 VU+ boxes slow responses on internal readers for T0/T1 protocol
    - T14 protocol was broken and now is handled like before and communicated again with reader driver
    
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@8578 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- csctapi/ifd_sci.c
+++ csctapi/ifd_sci.c
@@ -331,8 +331,12 @@ static int32_t sci_write_settings3(struct s_reader *reader, uint32_t ETU, uint32
 	if (reader->mhz > 2000){ // only for dreambox internal readers
 		// EGT = 0; // communicating guardtime is only slowing card ecm responses down. its not needed with internal readers, the drivers already take care of this!
 		// P fixed at 5V since this is default class A card, and TB is deprecated
-		call (Sci_WriteSettings (reader, 0, reader->divider, ETU, WWT, reader->CWT, reader->BWT, 0, 5, (unsigned char)I));
-	} else { // all other brand boxes than dreamboxes!
+		if (reader->protocol_type != ATR_PROTOCOL_TYPE_T14){ // fix VU+ internal reader slow responses on T0/T1
+			call (Sci_WriteSettings (reader, 0, reader->divider, ETU, WWT, reader->CWT, reader->BWT, 0, 5, (unsigned char)I));
+		} else { // no fixup for T14 protocol otherwise error
+			call (Sci_WriteSettings (reader, reader->protocol_type, reader->divider, ETU, WWT, reader->CWT, reader->BWT, 0, 5, (unsigned char)I));
+		}
+	} else { // all other brand boxes than dreamboxes or VU+!
 		// EGT = 0; // dont communicate guardtime -> slowing down card ecm responses!
 		// P fixed at 5V since this is default class A card, and TB is deprecated
 		call (Sci_WriteSettings (reader, reader->protocol_type, reader->mhz / 100, ETU, WWT, reader->CWT, reader->BWT, 0, 5, (unsigned char)I));
