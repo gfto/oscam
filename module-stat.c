80ea284c5d167f765ffad3eb003bb5fc177e84a7
theparasol <theparasol@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2013-05-24 14:21:33 +0000

    - LB Fix for viaccess multiple ecmpids on same caid/provid
    For other cas systems this was already fixed but not for viaccess.
    Now in case of caid 0x500 ecm[8] & ecm[9] is put into chid of stats.
    That way a not found on a certain ecmpid cant trigger srvid blacklisting.
    
    ecm[8]/ecm[9] isnt extensive tested so in case of issues report in forum! 
    
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@8660 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-stat.c
+++ module-stat.c
@@ -71,9 +71,10 @@ static uint32_t get_subid(ECM_REQUEST *er)
 	uint32_t id = 0;
 	switch (er->caid>>8)
 	{
-		case 0x01: id = b2i(2, er->ecm+7); break;
-		case 0x06: id = b2i(2, er->ecm+6); break;
-		case 0x09: id = b2i(2, er->ecm+11); break;
+		case 0x01: id = b2i(2, er->ecm+7); break; // seca
+		case 0x05: id = b2i(2, er->ecm+8); break; // viaccess
+		case 0x06: id = b2i(2, er->ecm+6); break; // irdeto
+		case 0x09: id = b2i(2, er->ecm+11); break; // videoguard
 		case 0x4A: // DRE-Crypt, Bulcrypt, others?
 			if (er->caid != 0x4AEE) // Bulcrypt
 				id = er->ecm[7];
