65ac1dfe210c11d8ab63f2f7cc58a705341f403e
gorgone.impertinence <gorgone.impertinence@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2013-08-24 12:26:09 +0000

    enlarge CS_MAXTUNTAB to 100
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@8860 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- globals.h
+++ globals.h
@@ -234,7 +234,7 @@ typedef unsigned char uchar;
 #endif
 #define CS_QLEN       128 // size of request queue
 #define CS_MAXCAIDTAB 32  // max. caid-defs/user
-#define CS_MAXTUNTAB  50  // max. betatunnel mappings
+#define CS_MAXTUNTAB  100  // max. betatunnel mappings
 #define CS_MAXPROV    32
 #define CS_MAXPORTS   32  // max server ports
 #define CS_MAXFILTERS   16
