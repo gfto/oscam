b064b866efb598f820f4220596ec25d8f62fd38a
stefansat <stefansat@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2013-10-15 09:42:06 +0000

    the file seemed not to be included in commit
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@8981 4b0bc96b-bc66-0410-9d44-ebda105a78c1

---
+++ csctapi/ifd_smartreader.h
@@ -0,0 +1,8 @@
+// FTDI defines
+#ifndef __SMARTREADER_H__
+#define __SMARTREADER_H__
+/** variable which is used in other modulels ass well **/
+int smartdev_found;
+
+#endif // __SMARTREADER_TYPES_H__
+// end of FTDI defines
