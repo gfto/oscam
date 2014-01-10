520f038225910720a48c97f312355f54c9cd63f9
gf <gf@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2014-01-10 11:34:24 +0000

    webif: Fix user name encoding in URLs.
    This should fix ticket #3579.
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@9242 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-webif.c
+++ module-webif.c
@@ -3110,31 +3110,17 @@ static char *send_oscam_user_config(struct templatevars *vars, struct uriparams 
 		}
 
 		tpl_addVar(vars, TPLADD, "CLASSNAME", classname);
+		tpl_addVar(vars, TPLADD, "USERNAME", xml_encode(vars, account->usr));
+		tpl_addVar(vars, TPLADD, "USERNAMEENC", urlencode(vars, account->usr));
+
+		static const char *user_tpl_name = "USERLABEL";
 		if(cfg.http_showpicons && !apicall)
-		{
-			if(picon_exists(xml_encode(vars, account->usr)))
-			{
-				tpl_addVar(vars, TPLADD, "USERICON", xml_encode(vars, account->usr));
-				tpl_addVar(vars, TPLADD, "USERTITLE", xml_encode(vars, account->usr));
-				tpl_addVar(vars, TPLADD, "USERNAME", xml_encode(vars, account->usr));
-				tpl_addVar(vars, TPLADD, "USER", tpl_getTpl(vars, "USERICON"));
-			}
-			else
-			{
-				tpl_addVar(vars, TPLADD, "USERNAME", xml_encode(vars, account->usr));
-				tpl_addVar(vars, TPLADD, "USER", tpl_getTpl(vars, "USERNOICON"));
-			}
-		}
-		else
-		{
-			tpl_addVar(vars, TPLADD, "USERNAME", xml_encode(vars, account->usr));
-			tpl_addVar(vars, TPLADD, "USER", tpl_getTpl(vars, "USERLABEL"));
-		}
+			user_tpl_name = picon_exists(xml_encode(vars, account->usr)) ? "USERICON" : "USERNOICON";
+		tpl_addVar(vars, TPLADD, "USERBIT", tpl_getTpl(vars, user_tpl_name));
 
 		char *value = mk_t_group(account->grp);
 		tpl_addVar(vars, TPLADD, "GROUPS", value);
 		free_mk_t(value);
-		tpl_addVar(vars, TPLADD, "USERENC", urlencode(vars, account->usr));
 		tpl_addVar(vars, TPLADD, "DESCRIPTION", xml_encode(vars, account->description ? account->description : ""));
 		tpl_addVar(vars, TPLADD, "STATUS", status);
 		tpl_addVar(vars, TPLAPPEND, "STATUS", expired);
