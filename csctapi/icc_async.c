gfto/oscam

commit 71f7fa3f28d036808ba560a9df93a5aa4a458a1f

- Fix for internal readers, seems they all need Fi instead of F parameter!
git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@8963 4b0bc96b-bc66-0410-9d44-ebda105a78c1

csctapi/icc_async.c
@@ -820,11 +820,11 @@ static int32_t InitCard(struct s_reader *reader, ATR *atr, unsigned char FI, uin
 	}
 	else if(reader->crdr.write_settings2)
 	{
-		call(reader->crdr.write_settings2(reader, (uint16_t) F, (uint8_t) D, WWT, EGT, BGT));
+		call(reader->crdr.write_settings2(reader, (uint16_t) Fi, (uint8_t) D, WWT, EGT, BGT));
 	}
 	else if(reader->crdr.write_settings3)
 	{
-		call(reader->crdr.write_settings3(reader, ETU, F, WWT, reader->CWT, reader->BWT, EGT, (unsigned char)I));
+		call(reader->crdr.write_settings3(reader, ETU, Fi, WWT, reader->CWT, reader->BWT, EGT, (unsigned char)I));
 	}
 
 	if(reader->typ == R_INTERNAL)

commit 0638d65d4c0d58c338997d567c5b5c6e68b3c1ac

Fix internal reader in DM500
The same workaround for base frequency is needed in DM500 (PPC).
Otherwise the card cannot init, with the following errors:
Resync error: readtimeouts 0/0 (max/min) us, writetimeouts 0/0 (max/min) us

Bisecting shows that the problem started with r8963.


git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@9407 4b0bc96b-bc66-0410-9d44-ebda105a78c1

csctapi/icc_async.c
@@ -872,8 +872,8 @@ static int32_t InitCard(struct s_reader *reader, ATR *atr, unsigned char FI, uin
 	}
 	else if(reader->crdr.write_settings3)
 	{
-#if defined(__SH4__) || defined(STB04SCI)
-		Fi = F/100; // workaround: SH4 internal reader needs base frequency like 1,2,3,4,5,6 Mhz not clock rate conversion factor (Fi) 
+#if defined(__SH4__) || defined(STB04SCI) || defined(__powerpc__)
+		Fi = F/100; // workaround: SH4/DM500 internal reader needs base frequency like 1,2,3,4,5,6 MHz not clock rate conversion factor (Fi)
 #endif
 		call(reader->crdr.write_settings3(reader, ETU, Fi, WWT, reader->CWT, reader->BWT, EGT, (unsigned char)I));
 	}