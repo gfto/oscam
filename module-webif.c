43bfab32f4d707c958ebf64839a7ce94774f2487
gorgone.impertinence <gorgone.impertinence@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2014-01-19 03:02:56 +0000

    use urlencode for run as
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@9400 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-webif.c
+++ module-webif.c
@@ -6618,7 +6618,7 @@ static int32_t process_request(FILE * f, IN_ADDR_T in)
 			tpl_printf(vars, TPLADD, "STARTDATE", "%02d.%02d.%02d", st.tm_mday, st.tm_mon + 1, st.tm_year % 100);
 			tpl_printf(vars, TPLADD, "STARTTIME", "%02d:%02d:%02d", st.tm_hour, st.tm_min, st.tm_sec);
 			tpl_printf(vars, TPLADD, "PROCESSID", "%d", getpid());
-			tpl_printf(vars, TPLADD, "RUNAS", "%s", username(first_client));
+			tpl_printf(vars, TPLADD, "RUNAS", "%s", urlencode(vars, username(first_client)));
 
 			time_t now = time((time_t *)0);
 			// XMLAPI
