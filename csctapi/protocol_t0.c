897399936a69df501399210a7250205496a6cedb
stefansat <stefansat@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2014-01-19 16:08:23 +0000

    smartreader removed delays for T14 was not cause of error
    Removed also extra log outputs
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@9406 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- csctapi/protocol_t0.c
+++ csctapi/protocol_t0.c
@@ -528,22 +528,22 @@ int32_t Protocol_T14_ExchangeTPDU(struct s_reader *reader, unsigned char *cmd_ra
 
 	/* Send apdu */
 	timeout = ICC_Async_GetTimings(reader, reader->char_delay);  // we are going to send: char delay timeout
-	if(ICC_Async_Transmit(reader, cmd_len + 2, 0, buffer, 0, timeout) != OK) { rdr_log(reader," T14 transmit timeout error"); return ERROR; }   //send apdu
+	if(ICC_Async_Transmit(reader, cmd_len + 2, 0, buffer, 0, timeout) != OK) { return ERROR; }   //send apdu
 	if(cmd_raw[0] == 0x02 && cmd_raw[1] == 0x09) { cs_sleepms(2500); }  //FIXME why wait? -> needed for init on overclocked T14 cards
 
 	timeout = ICC_Async_GetTimings(reader, reader->read_timeout);  // we are going to receive: WWT timeout
-	if ((reader->typ == R_SMART) && (reader->smart_type >= 2)) timeout *= 1.1;
-	if(ICC_Async_Receive(reader, 8, buffer, 0, timeout) != OK) { rdr_log(reader, " DAMNED STILL T14 receive timeout error 1"); return ERROR; }   //Read one procedure byte
+//	if ((reader->typ == R_SMART) && (reader->smart_type >= 2)) timeout *= 1.1;
+	if(ICC_Async_Receive(reader, 8, buffer, 0, timeout) != OK) { return ERROR; }   //Read one procedure byte
 	recved = (int32_t)buffer[7];
 	if(recved)
 	{
 		timeout = ICC_Async_GetTimings(reader, reader->read_timeout);  // we are going to receive: WWT timeout
-		if ((reader->typ == R_SMART) && (reader->smart_type >= 2)) timeout *= 1.1;
-		if(ICC_Async_Receive(reader, recved, buffer + 8, 0 , timeout) != OK) { rdr_log(reader, " DAMNED STILL T14 receive timeout error 2"); return ERROR; }
+//		if ((reader->typ == R_SMART) && (reader->smart_type >= 2)) timeout *= 1.1;
+		if(ICC_Async_Receive(reader, recved, buffer + 8, 0 , timeout) != OK) {return ERROR; }
 	}
 	timeout = ICC_Async_GetTimings(reader, reader->read_timeout);  // we are going to receive: WWT timeout
-	if ((reader->typ == R_SMART) && (reader->smart_type >= 2)) timeout *= 1.1;
-	if(ICC_Async_Receive(reader, 1, &ixor, 0, timeout) != OK) { rdr_log(reader, " DAMNED STILL T14 receive timeout error 3"); return ERROR; }
+//	if ((reader->typ == R_SMART) && (reader->smart_type >= 2)) timeout *= 1.1;
+	if(ICC_Async_Receive(reader, 1, &ixor, 0, timeout) != OK) {return ERROR; }
 	for(i = 0; i < 8 + recved; i++)
 		{ ixor1 ^= buffer[i]; }
 	if(ixor1 != ixor)
