70c5211c8ee215caad41fb56aba8e29a6fb1bb7d
skyndas <skyndas@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2014-01-26 21:32:24 +0000

    WebIf:
    - Fix some typo in HTML
    - Added uncompressed script for searching in table with fix for two TH in THEAD of TABLE )
    - Remove script for sorting and added better uncompressed script for sorting ( so far, only for USERS )
    
    We sorting in USERS by Label, Protocol, LTIME, ECM OK, ECM NOK, ECM IGN, ECM TOUT, ECM, CACHE, ECM TUN, ECM last 60s, EMM OK, EMM NOK, CW Rate, CASC USERS.
    
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@9454 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- webif/include/jscript.js
+++ webif/include/jscript.js
@@ -1,9 +1,128 @@
+/* Function for refresh pages */
 var oReloadTimer=null;var oCounterTimer=null;
 function reloadDocument(){history.pushState('', document.title, window.location.pathname);window.location.reload();};
-function cdpause(){clearTimeout(oReloadTimer);};
-function addreader(){cdpause();document.getElementById("searchTable").style.display="none";document.getElementById("newreader").style.display="block";};
-function adduser(){cdpause();document.getElementById("searchTable").style.display="none";document.getElementById("newuser").style.display="block";};
+function reloadPause(){clearTimeout(oReloadTimer);};
 function runReloadCounter(){var oReloadContent=document.getElementById("ReloadContent");if(oReloadContent){if(counter<0){counter=counterfull;}counter--;}};
 function initDoc(){if(oReloadTimer)window.clearInterval(oReloadTimer);oReloadTimer=window.setInterval("reloadDocument();",counterfull*1000);if(oCounterTimer)window.clearInterval(oCounterTimer);oCounterTimer=window.setInterval("runReloadCounter();",1000);};
-function doSearch(){var searchText=document.getElementById('searchTerm').value;var targetTable=document.getElementById('dataTable');var targetTableColCount;for(var rowIndex=0;rowIndex<targetTable.rows.length;rowIndex++){var rowData='';if(rowIndex==1){targetTableColCount=targetTable.rows.item(rowIndex).cells.length;continue}for(var colIndex=0;colIndex<targetTableColCount;colIndex++){var cellText='';if(navigator.appName=='Microsoft Internet Explorer')cellText=targetTable.rows.item(rowIndex).cells.item(colIndex).innerText;else cellText=targetTable.rows.item(rowIndex).cells.item(colIndex).textContent;rowData+=cellText}rowData=rowData.toLowerCase();searchText=searchText.toLowerCase();if(rowData.indexOf(searchText)==-1)targetTable.rows.item(rowIndex).style.display='none';else targetTable.rows.item(rowIndex).style.display='table-row';}};
-(function(document) {'use strict';var LightTableSorter = (function(Arr) {var _th, _cellIndex, _order = '';function _text(row) {return row.cells.item(_cellIndex).textContent.toLowerCase();}function _sort(a, b) {var va = _text(a), vb = _text(b), n = parseInt(va, 10);if (n) {va = n;vb = parseInt(vb, 10);}return va > vb ? 1 : va < vb ? -1 : 0;}function _toggle() {var c = _order !== 'asc' ? 'asc' : 'desc';_th.className = (_th.className.replace(_order, '') + ' ' + c).trim();_order = c;}function _reset() {_th.className = _th.className.replace('asc', '').replace('desc', '');_order = '';}function onClickEvent(e) {if (_th && _cellIndex !== e.target.cellIndex) {_reset();}_th = e.target;_cellIndex = _th.cellIndex;var tbody = _th.offsetParent.getElementsByTagName('tbody')[0],rows = tbody.rows;if (rows) {rows = Arr.sort.call(Arr.slice.call(rows, 0), _sort);if (_order === 'asc') {Arr.reverse.call(rows);}_toggle();tbody.innerHtml = '';Arr.forEach.call(rows, function(row) { tbody.appendChild(row); });}}return {init: function() {var ths = document.getElementsByTagName('th');Arr.forEach.call(ths, function(th) { th.onclick = onClickEvent; });}};})(Array.prototype);document.addEventListener('readystatechange', function() {if (document.readyState === 'complete') {LightTableSorter.init();}});})(document);
+
+/* Function for add new user in readers.html */
+function addreader(){cdpause();document.getElementById("searchTable").style.display="none";document.getElementById("newreader").style.display="block";};
+
+/* Function for add new reader in userconfig.html */
+function adduser(){cdpause();document.getElementById("searchTable").style.display="none";document.getElementById("newuser").style.display="block";};
+
+/* Function for searching in table - uncompressed */
+function doSearch() {
+	var searchText = document.getElementById('searchTerm').value;
+	var targetTable = document.getElementById('dataTable');
+	var targetTableColCount;
+
+	//Loop through table rows
+	for (var rowIndex = 0; rowIndex < targetTable.rows.length; rowIndex++) {
+		var rowData = '';
+		//Get column count from header row
+		if (rowIndex == 0 || rowIndex==1) {
+			targetTableColCount = targetTable.rows.item(rowIndex).cells.length;
+			continue; //do not execute further code for header row.
+		}
+
+		//Process data rows. (rowIndex >= 1)
+		for (var colIndex = 0; colIndex < targetTableColCount; colIndex++) {
+			var cellText = '';
+			if (navigator.appName == 'Microsoft Internet Explorer')
+				cellText = targetTable.rows.item(rowIndex).cells.item(colIndex).innerText;
+			else
+			cellText = targetTable.rows.item(rowIndex).cells.item(colIndex).textContent;
+
+			rowData += cellText;
+		}
+
+		// Make search case insensitive.
+		rowData = rowData.toLowerCase();
+		searchText = searchText.toLowerCase();
+
+		//If search term is not found in row data
+		//then hide the row, else show
+		if (rowData.indexOf(searchText) == -1)
+			targetTable.rows.item(rowIndex).style.display = 'none';
+		else
+			targetTable.rows.item(rowIndex).style.display = 'table-row';
+		}
+	}
+
+/* Functions for sorting in table - uncompressed */
+var TINY={};
+
+function T$(i){return document.getElementById(i)}
+function T$$(e,p){return p.getElementsByTagName(e)}
+
+TINY.table=function(){
+	function sorter(n,t,p){this.n=n; this.id=t; this.p=p; if(this.p.init){this.init()}}
+	sorter.prototype.init=function(){
+		this.set(); var t=this.t, i=d=0; t.h=T$$('tr',t)[1];
+		t.l=t.r.length; t.w=t.r[0].cells.length; t.a=[]; t.c=[];
+		if(this.p.colddid){
+			d=T$(this.p.colddid);
+			var o = document.createElement('option');
+			o.value = -1; 
+            o.innerHTML = 'All Columns'; 
+            d.appendChild(o)
+		}
+		for(i;i<t.w;i++){
+			var c=t.h.cells[i]; t.c[i]={};
+			if(c.className!='nosort'){
+			    c.className = this.p.headclass; 
+                c.onclick=new Function(this.n+'.sort('+i+')');
+				c.onmousedown=function(){return false};
+			}
+			if(this.p.columns){
+				var l=this.p.columns.length, x=0;
+				for(x;x<l;x++){
+					if(this.p.columns[x].index==i){
+						var g=this.p.columns[x];
+						t.c[i].format=g.format==null?1:g.format; t.c[i].decimals=g.decimals==null?2:g.decimals
+					}
+				}
+			}
+			if(d){
+			    var o = document.createElement('option');
+			    o.value = i; o.innerHTML = T$$('h3', c)[0].innerHTML; 
+                d.appendChild(o)
+			}
+		}
+		this.reset()
+	};
+	sorter.prototype.reset=function(){
+		var t=this.t; t.t=t.l;
+		for(var i=0;i<t.l;i++){t.a[i]={}; t.a[i].s=1}
+		if(this.p.sortcolumn!=undefined){
+			this.sort(this.p.sortcolumn,1,this.p.is)
+		}
+	};
+	sorter.prototype.sort=function(x,f,z){
+		var t=this.t; t.y=x; var x=t.h.cells[t.y], i=0, n=document.createElement('tbody');
+		for(i;i<t.l;i++){
+			t.a[i].o=i; var v=t.r[i].cells[t.y]; t.r[i].style.display='';
+			while(v.hasChildNodes()){v=v.firstChild}
+			t.a[i].v=v.nodeValue?v.nodeValue:''
+		}
+		for(i=0;i<t.w;i++){var c=t.h.cells[i]; if(c.className!='nosort'){c.className=this.p.headclass}}
+		if(t.p==t.y&&!f){t.a.reverse(); x.className=t.d?this.p.ascclass:this.p.descclass; t.d=t.d?0:1}
+		else{t.p=t.y; f&&this.p.sortdir==-1?t.a.sort(cp).reverse():t.a.sort(cp); t.d=0; x.className=this.p.ascclass}
+		for(i=0;i<t.l;i++){var r=t.r[t.a[i].o].cloneNode(true); n.appendChild(r)}
+		t.replaceChild(n,t.b); this.set();
+	};
+	sorter.prototype.set=function(){
+		var t=T$(this.id); t.b=T$$('tbody',t)[0]; t.r=t.b.rows; this.t=t
+	};
+	function decimals(n,d){return Math.round(n*Math.pow(10,d))/Math.pow(10,d)};
+	function cp(f,c){
+		var g,h; f=g=f.v.toLowerCase(); c=h=c.v.toLowerCase();
+		var i=parseFloat(f.replace(/(\$|\,)/g,'')), n=parseFloat(c.replace(/(\$|\,)/g,''));
+		if(!isNaN(i)&&!isNaN(n)){g=i,h=n}
+		i=Date.parse(f); n=Date.parse(c);
+		if(!isNaN(i)&&!isNaN(n)){g=i; h=n}
+		return g>h?1:(g<h?-1:0)
+	};
+	return{sorter:sorter}
+}();
