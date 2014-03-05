fe4578658e79c8e1383941ede33f0290a992900b
Deas <Deas@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2014-03-05 22:30:19 +0000

    add patch series by gismo2004: whitelist - for details see: ​http://www.streamboard.tv/wbb2/thread.php?postid=502478#post502478
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@9563 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-stat.c
+++ module-stat.c
@@ -1823,7 +1823,7 @@ static int8_t add_to_ecmlen(struct s_reader *rdr, READER_STAT *s)
 
 void update_ecmlen_from_stat(struct s_reader *rdr)
 {
-	if(!rdr || &rdr->lb_stat)
+	if(!rdr || !&rdr->lb_stat)
 		{ return; }
 
 	cs_readlock(&rdr->lb_stat_lock);
