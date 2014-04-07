7bad0363b298e6a3c851c64d401a50e2203b7f5a
puya <puya@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2014-04-07 12:08:03 +0000

    Fix Possible SEGFAULT
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@9634 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-cccam.c
+++ module-cccam.c
@@ -2428,6 +2428,7 @@ void cc_cache_push_in(struct s_client *cl, uchar *buf)
 	cs_debug_mask(D_CACHEEX, "cacheex: received %d nodes %s", (int32_t)count, username(cl));
 	//Read lastnodes:
 	uint8_t *data;
+	if (er)
 	er->csp_lastnodes = ll_create("csp_lastnodes");
 	while(count)
 	{
