81a2dc6c11a84c440567b00550da32e11cbcc991
spdfrk1 <spdfrk1@github>
2014-07-08 23:52:23 +0200

    constcw reader sacrificed for testing purposes

--- module-constcw.c
+++ module-constcw.c
@@ -6,155 +6,218 @@
 #include "oscam-net.h"
 #include "oscam-string.h"
 
+#include "openssl/aes.h"
+
+#define OFFSET_MKEY1 4
+#define OFFSET_MKEY2 56
+#define OFFSET_EXPIRE_MKEY1 36
+#define OFFSET_EXPIRE_MKEY2 88
+
+#define OFFSET_CWKEY1 33
+#define OFFSET_CWKEY2 49
+
+#define touInt16(__data) (((&__data)[1] << 8) | __data)
+
 static int32_t pserver;
 
-int32_t constcw_file_available(void)
-{
-	FILE *fp;
+unsigned long parse_ts(uchar * data) {
+struct tm t;
+time_t t_of_day;
+t.tm_year = touInt16(data[0]) - 1900; //2014-1900
+t.tm_mon = touInt16(data[2]) - 1; // Month, 0 - jan
+t.tm_mday = touInt16(data[4]); // Day of the month
+t.tm_hour = touInt16(data[6]);
+t.tm_min = touInt16(data[8]);
+t.tm_sec = touInt16(data[10]);
+t.tm_isdst = -1; // Is DST on? 1 = yes, 0 = no, -1 = unknown
+t_of_day = mktime(&t);
 
-	fp = fopen(cur_client()->reader->device, "r");
-	if(!fp) { return (0); }
-	fclose(fp);
-	return (1);
+return (unsigned long) t_of_day;
 }
 
-int32_t constcw_analyse_file(uint16_t c_caid, uint32_t c_prid, uint16_t c_sid, uint16_t c_pmtpid, uint32_t c_vpid, uint16_t c_ecmpid, uchar *dcw)
-{
-	//CAID:PROVIDER:SID:PMTPID:ECMPID:VPID:XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX
-	FILE *fp;
-	char token[512];
-	uint32_t caid, provid, sid, vpid, pmtpid, ecmpid;
-	int32_t cw[16];
+int32_t constcw_file_available(void) {
+    FILE *fp;
 
-	fp = fopen(cur_client()->reader->device, "r");
-	if(!fp) { return (0); }
+    fp = fopen(cur_client()->reader->device, "r");
+    if (!fp) {
+        return (0);
+    }
+    fclose(fp);
+    return (1);
+}
 
-	cs_log("[CONSTCW] find controlword for CAID %04X PROVID %06X SRVID %04X ECMPID %04X PMTPID %04X VPID %04X", c_caid, c_prid, c_sid, c_ecmpid, c_pmtpid, c_vpid);
+int32_t constcw_analyse_file(uchar * dcw, uchar * ECM) {
+    FILE *fp;
+    uchar token[108];
+    uchar * mkey;
+    uint32_t t = 0;
+    AES_KEY aesmkey;
+    uchar table = ECM[0];
+    uint16_t channel = (ECM[18] << 8) + ECM[19];
+    unsigned long time_now, time_mkey1, time_mkey2;
+    fp = fopen(cur_client()->reader->device, "r");
+    if (!fp) {
+        cs_log("[CONSTKB] Could not open file");
+        return (0);
+    }
+    cs_log("[CONSTKB] Find control word for Channel %d table 0x%02X", channel,
+            table);
 
-	while(fgets(token, sizeof(token), fp))
-	{
-		if(token[0] == '#') { continue; }
-		vpid = 0;
-		int ret = sscanf(token, "%4x:%6x:%4x:%4x:%4x::%2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x", &caid, &provid, &sid, &pmtpid, &ecmpid,
-			   &cw[0], &cw[1], &cw[2], &cw[3], &cw[4], &cw[5], &cw[6], &cw[7],
-			   &cw[8], &cw[9], &cw[10], &cw[11], &cw[12], &cw[13], &cw[14], &cw[15]);
-		
-		if(ret != 21){   
-			ret = sscanf(token, "%4x:%6x:%4x:%4x:%4x:%4x:%2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x", &caid, &provid, &sid, &pmtpid, &ecmpid, &vpid,
-				   &cw[0], &cw[1], &cw[2], &cw[3], &cw[4], &cw[5], &cw[6], &cw[7],
-				   &cw[8], &cw[9], &cw[10], &cw[11], &cw[12], &cw[13], &cw[14], &cw[15]);
-			if(ret != 22) continue;
-		}		
+    fseek(fp, 4, SEEK_SET);
+    while (fread(token, 108, 1, fp)) {
+        if ((uint16_t) ((token[t + 1] << 8) + token[t]) == channel) {
+            cs_log("[CONSTKB] Master keys found for Channel: %d", channel);
+            time_now = (unsigned long)time(NULL);
+            time_mkey1 = parse_ts(token + OFFSET_EXPIRE_MKEY1);
+                        
+            if (difftime(time_mkey1, time_now) > 0) { // Check expire date mkey 1
+                cs_log("[CONSTKB] Master key 1 selected");
+                mkey = token + OFFSET_MKEY1;
+            } else {                
+                time_mkey2 = parse_ts(token + OFFSET_EXPIRE_MKEY2);        
+                if (difftime(time_mkey2, time_now) > 0) { // Check expire date mkey 2
+                    cs_log("[CONSTKB] Master key 2 selected");
+                    if(difftime(time_mkey2, time_now) < 86400) {
+                        cs_log("[CONSTKB] Warning: Master keys for Channel: %d will expire in %lu seconds", channel, (unsigned long)difftime(time_mkey2, time_now));
+                    }
+                    mkey = token + OFFSET_MKEY2;
+                } else {
+                    cs_log("[CONSTKB] Keyblock is to old");
+                    return 0;
+                }
+            }
+            AES_set_decrypt_key(mkey, 128, &aesmkey);
 
-		//cs_log("Line found: %s", token);
-		if(c_caid == caid && c_sid == sid && (!provid || provid == c_prid) && (!pmtpid || !c_pmtpid || pmtpid == c_pmtpid) && (!vpid || !c_vpid || vpid == c_vpid) 
-				&& (!ecmpid || !c_ecmpid || ecmpid == c_ecmpid))
-		{
-			fclose(fp);
-			int8_t i;
-			for(i = 0; i < 16; ++i)
-				{ dcw[i] = (uchar) cw[i]; }
-			cs_log("[CONSTCW] Entry found: %04X:%06X:%04X:%04X:%04X:%04X:%s", caid, provid, sid, pmtpid, ecmpid, vpid, cs_hexdump(1, dcw, 16, token, sizeof(token)));
-			return 1;
-		}
-	}
-
-	fclose(fp);
-	return 0;
+            for (t = 0; t < 48; t += 16) {
+                AES_ecb_encrypt(&ECM[24 + t], &ECM[24 + t], &aesmkey,
+                AES_DECRYPT);
+            }
+            if (memcmp(&ECM[24], "CEB", 3) == 0) {
+                cs_log("[CONSTKB] ECM decrypt check passed");
+            } else {
+                cs_log(
+                        "[CONSTKB] ECM decrypt failed, wrong master key or unknown format");
+                fclose(fp);
+                return 0;
+            }
+            if (table == 0x80) {
+                for (t = 0; t < 16; t++) {
+                    dcw[t] = ECM[t + OFFSET_CWKEY1];
+                }
+            }
+            else {
+                for (t = 0; t < 16; t++) {
+                    dcw[t] = ECM[t + OFFSET_CWKEY2];
+                }
+            }
+            fclose(fp);
+            return 1;
+        }
+    }
+    cs_log("[CONSTKB] No Master key found for channel: %d, cannot decrypt ECM",
+            channel);
+    fclose(fp);
+    return 0;
 }
 //************************************************************************************************************************
 //* client/server common functions
 //************************************************************************************************************************
-static int32_t constcw_recv(struct s_client *client, uchar *buf, int32_t l)
-{
-	int32_t ret;
+static int32_t constcw_recv(struct s_client *client, uchar *buf, int32_t l) {
+    int32_t ret;
 
-	if(!client->udp_fd) { return (-9); }
-	ret = read(client->udp_fd, buf, l);
-	if(ret < 1) { return (-1); }
-	client->last = time(NULL);
-	return (ret);
+    if (!client->udp_fd) {
+        return (-9);
+    }
+    ret = read(client->udp_fd, buf, l);
+    if (ret < 1) {
+        return (-1);
+    }
+    client->last = time(NULL);
+    return (ret);
 }
 
 //************************************************************************************************************************
 //*       client functions
 //************************************************************************************************************************
-int32_t constcw_client_init(struct s_client *client)
-{
-	int32_t fdp[2];
+int32_t constcw_client_init(struct s_client *client) {
+    int32_t fdp[2];
+    FILE *fp;
+    uint32_t tokens;
+    uchar channels[4];
 
-	client->pfd = 0;
-	if(socketpair(PF_LOCAL, SOCK_STREAM, 0, fdp))
-	{
-		cs_log("[CONSTCW] Socket creation failed (%s)", strerror(errno));
-		return 1;
-	}
-	client->udp_fd = fdp[0];
-	pserver = fdp[1];
+    client->pfd = 0;
+    if (socketpair(PF_LOCAL, SOCK_STREAM, 0, fdp)) {
+        cs_log("[CONSTKB] Socket creation failed (%s)", strerror(errno));
+        return 1;
+    }
+    client->udp_fd = fdp[0];
+    pserver = fdp[1];
 
-	memset((char *) &client->udp_sa, 0, sizeof(client->udp_sa));
-	SIN_GET_FAMILY(client->udp_sa) = AF_INET;
+    memset((char *) &client->udp_sa, 0, sizeof(client->udp_sa));
+    SIN_GET_FAMILY(client->udp_sa) = AF_INET;
 
-	// Oscam has no reader.au in s_reader like ki's mpcs ;)
-	// reader[ridx].au = 0;
-	// cs_log("local reader: %s (file: %s) constant cw au=0", reader[ridx].label, reader[ridx].device);
-	cs_log("[CONSTCW] local reader: %s (file: %s)", client->reader->label, client->reader->device);
+    cs_log("[CONSTKB] local reader: %s (file: %s)", client->reader->label,
+            client->reader->device);
 
-	client->pfd = client->udp_fd;
+    client->pfd = client->udp_fd;
 
-	if(constcw_file_available())
-	{
-		client->reader->tcp_connected = 2;
-		client->reader->card_status = CARD_INSERTED;
-	}
+    fp = fopen(cur_client()->reader->device, "r");
+    if (!fp) {
+        return (1);
+    }
 
-	return (0);
+    if (fread(channels, 4, 1, fp) < 1) {
+        cs_log("[CONSTKB] Unable to read from file");
+        fclose(fp);
+        return 1;
+    }
+    tokens = (channels[3] << 24) + (channels[2] << 16) + (channels[1] << 8)
+            + channels[0];
+
+    cs_log("[CONSTKB] Channels expected in block: %d", tokens);
+
+    client->reader->tcp_connected = 2;
+    client->reader->card_status = CARD_INSERTED;
+
+    fclose(fp);
+    return (0);
 }
 
-static int32_t constcw_send_ecm(struct s_client *client, ECM_REQUEST *er, uchar *UNUSED(msgbuf))
-{
-	time_t t;
-	struct s_reader *rdr = client->reader;
-	uchar cw[16];
+static int32_t constcw_send_ecm(struct s_client *client, ECM_REQUEST *er,
+        uchar *UNUSED(msgbuf)) {
+    time_t t;
+    struct s_reader *rdr = client->reader;
+    uchar cw[16];
 
-	t = time(NULL);
-	// Check if DCW exist in the files
-	//cs_log("Searching ConstCW for ECM: %04X:%06X:%04X (%d)", er->caid, er->prid, er->srvid, er->l);
+    t = time(NULL);
+    // Check if DCW exist in the files
+    if (constcw_analyse_file(cw, er->ecm) == 0) {
+        write_ecm_answer(rdr, er, E_NOTFOUND, (E1_READER << 4 | E2_SID), NULL,
+        NULL);
+    } else {
+        write_ecm_answer(rdr, er, E_FOUND, 0, cw, NULL);
+    }
 
-	if(constcw_analyse_file(er->caid, er->prid, er->srvid, er->pmtpid, er->vpid, er->pid, cw) == 0)
-	{
-		write_ecm_answer(rdr, er, E_NOTFOUND, (E1_READER << 4 | E2_SID), NULL, NULL);
-	}
-	else
-	{
-		write_ecm_answer(rdr, er, E_FOUND, 0, cw, NULL);
-	}
-
-	client->last = t;
-	rdr->last_g = t;
-	return (0);
+    client->last = t;
+    rdr->last_g = t;
+    return (0);
 }
 
-static int32_t constcw_recv_chk(struct s_client *UNUSED(client), uchar *UNUSED(dcw), int32_t *rc, uchar *UNUSED(buf), int32_t UNUSED(n))
-{
-	//dcw = dcw;
-	//n = n;
-	//buf = buf;
-
-	*rc = 0;
-	return (-1);
+static int32_t constcw_recv_chk(struct s_client *UNUSED(client),
+        uchar *UNUSED(dcw), int32_t *rc, uchar *UNUSED(buf), int32_t UNUSED(n)) {
+    *rc = 0;
+    return (-1);
 }
 
-void module_constcw(struct s_module *ph)
-{
-	ph->desc = "constcw";
-	ph->type = MOD_NO_CONN;
-	ph->listenertype = LIS_CONSTCW;
-	ph->recv = constcw_recv;
+void module_constcw(struct s_module *ph) {
+    ph->desc = "CONSTKB";
+    ph->type = MOD_NO_CONN;
+    ph->listenertype = LIS_CONSTCW;
+    ph->recv = constcw_recv;
 
-	ph->c_init = constcw_client_init;
-	ph->c_recv_chk = constcw_recv_chk;
-	ph->c_send_ecm = constcw_send_ecm;
-	ph->num = R_CONSTCW;
+    ph->c_init = constcw_client_init;
+    ph->c_recv_chk = constcw_recv_chk;
+    ph->c_send_ecm = constcw_send_ecm;
+    ph->num = R_CONSTCW;
 }
 #endif
