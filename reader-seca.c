dc16f3ff7a94b2cfeb8a29376db7c0659d693b06
theparasol <theparasol@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2014-09-18 20:00:42 +0000

    - Another tryfix for ticket 3887 (nagra handshake crashes pcsc readers)
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@9853 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- reader-seca.c
+++ reader-seca.c
@@ -182,8 +182,8 @@ static int32_t seca_card_init(struct s_reader *reader, ATR *newatr)
 	uchar buf[256];
 	static const uchar ins0e[] = { 0xc1, 0x0e, 0x00, 0x00, 0x08 }; // get serial number (UA)
 	static const uchar ins16[] = { 0xc1, 0x16, 0x00, 0x00, 0x06 }; // get nr. of providers
-	static unsigned char ins80[] = { 0x80, 0xCA, 0x00, 0x00, 0x11 }; // switch to nagra layer
-	static unsigned char insEE[] = { 0xEE, 0x51, 0xDC, 0xB8, 0x4A, 0x1C, 0x15, 0x05, 0xB5, 0xA6, 0x9B, 0x91, 0xBA, 0x33, 0x19, 0xC4, 0x10 }; // some more nagra magic
+	static const uchar ins80[] = { 0x80, 0xCA, 0x00, 0x00, 0x11 }; // switch to nagra layer
+	static const uchar handshake[] = { 0xEE, 0x51, 0xDC, 0xB8, 0x4A, 0x1C, 0x15, 0x05, 0xB5, 0xA6, 0x9B, 0x91, 0xBA, 0x33, 0x19, 0xC4, 0x10 }; // nagra handshake
 	int32_t i;
 
 	cs_clear_entitlement(reader);
@@ -245,44 +245,23 @@ static int32_t seca_card_init(struct s_reader *reader, ATR *newatr)
 	if (((atr[7] << 8 | atr[8]) == 0x7070) && ((atr[9] &0x0F) >= 10)) // is this possibly a nagra card tunneling seca commands?
 	{
 		rdr_log(reader, "Trying to switch to nagra layer of this card!");
-		int32_t timeout = ICC_Async_GetTimings(reader, reader->char_delay); // going to send so char delay
-		ICC_Async_Transmit(reader, sizeof(ins80), 1, ins80, 0, timeout); // try to init nagra layer
-		timeout = ICC_Async_GetTimings(reader, reader->read_timeout);  // we are going to receive: WWT timeout 
-		ICC_Async_Receive(reader, 1, cta_res, 0, timeout); // fetch response 
-		if(cta_res[0] ==  0xCA)
+		write_cmd(ins80, handshake); // try to init nagra layer
+		if(cta_res[0] == 0x61 && cta_res[1] == 0x10)
 		{
-			rdr_log(reader, "Sending nagra handshake!");
-			timeout = ICC_Async_GetTimings(reader, reader->char_delay); // going to send so char delay
-			ICC_Async_Transmit(reader, sizeof(insEE), 2, insEE, 0, timeout); // nagra handshake (?)
-			timeout = ICC_Async_GetTimings(reader, reader->read_timeout);  // we are going to receive: WWT timeout 
-			ICC_Async_Receive(reader, 2, cta_res, 0, timeout); // fetch response
-			{
-				if(cta_res[0] == 0x61 && cta_res[1] == 0x10)
-				{
-					rdr_log(reader, "Fetching nagra ATR!");
-					ATR nagra_atr;
-					ICC_Async_Activate(reader, &nagra_atr, reader->deprecated); // get nagra atr
-					if(reader->crdr.unlock) { reader->crdr.unlock(reader); }
-					
-					unsigned char table_hist[ATR_MAX_HISTORICAL];
-					uint32_t table_hist_size; 
-					ATR_GetHistoricalBytes(&nagra_atr, table_hist, &table_hist_size); // get historical bytes containing romrev from nagra atr 
-					memset(reader->rom, 0, sizeof(reader->rom));
-					memcpy(reader->rom, table_hist, (table_hist_size <= sizeof(reader->rom) ? table_hist_size : sizeof(reader->rom))); // save historical bytes into reader romrev
-					rdr_log(reader, "This is a nagra card %s tunnelling seca", table_hist);
-					rdr_log(reader, "Switching back to seca mode!");
-					ICC_Async_Activate(reader, &nagra_atr, reader->deprecated); // switch back to seca layer
-					if(reader->crdr.unlock) { reader->crdr.unlock(reader); } 
-				}
-				else
-				{
-					rdr_log(reader, "Nagra handshake failed!");
-				}
-			}
+			rdr_log(reader, "Fetching nagra ATR!");
+			ATR nagra_atr;
+			call(reader->crdr.activate(reader, &nagra_atr)); // get nagra atr		
+			uint32_t hist_size; 
+			memset(reader->rom, 0, sizeof(reader->rom));
+			
+			ATR_GetHistoricalBytes(&nagra_atr, reader->rom, &hist_size); // get historical bytes containing romrev from nagra atr
+			
+			rdr_log(reader, "Switching back to seca mode!");
+			call(reader->crdr.activate(reader, &nagra_atr)); // switch back to seca layer
 		}
 		else
 		{
-			rdr_log(reader, "Switching to nagra layer of this card failed!");
+			rdr_log(reader, "Nagra handshake failed!");
 		}
 	}
 	
