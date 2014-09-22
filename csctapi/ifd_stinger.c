b21e1d1499845418011eedd6df5bc5110de540b0
felixka <felixka@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2014-09-22 15:02:55 +0000

    Removed gpio code from ifd_stinger.c. Makes no sense for an usb reader. I assume it has been copied from ifd_phoenix.c without understanding the functionality.
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@9860 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- csctapi/ifd_stinger.c
+++ csctapi/ifd_stinger.c
@@ -37,41 +37,7 @@ services = services2
 #define ERROR 1
 #define STINGER_DELAY 150
 
-#define GPIO_PIN (1 << (reader->detect - 4))
-
 static int32_t Stinger_Set_Clock(struct s_reader *reader, unsigned int c);
-
-static inline int reader_use_gpio(struct s_reader *reader)
-{
-	return reader->use_gpio && reader->detect > 4;
-}
-
-static void set_gpio(struct s_reader *reader, int32_t level)
-{
-	int ret = 0;
-
-	ret |= read(reader->gpio_outen, &reader->gpio, sizeof(reader->gpio));
-	reader->gpio |= GPIO_PIN;
-	ret |= write(reader->gpio_outen, &reader->gpio, sizeof(reader->gpio));
-
-	ret |= read(reader->gpio_out, &reader->gpio, sizeof(reader->gpio));
-	if(level > 0)
-		{ reader->gpio |= GPIO_PIN; }
-	else
-		{ reader->gpio &= ~GPIO_PIN; }
-	ret |= write(reader->gpio_out, &reader->gpio, sizeof(reader->gpio));
-
-	rdr_debug_mask(reader, D_IFD, "%s level: %d ret: %d", __func__, level, ret);
-}
-
-static void set_gpio_input(struct s_reader *reader)
-{
-	int ret = 0;
-	ret |= read(reader->gpio_outen, &reader->gpio, sizeof(reader->gpio));
-	reader->gpio &= ~GPIO_PIN;
-	ret |= write(reader->gpio_outen, &reader->gpio, sizeof(reader->gpio));
-	rdr_debug_mask(reader, D_IFD, "%s ret:%d", __func__, ret);
-}
 
 static bool Stinger_IO_Serial_WaitToWrite(struct s_reader *reader, uint32_t delay_us, uint32_t timeout_us)
 {
@@ -279,17 +245,6 @@ int32_t Stinger_Init(struct s_reader *reader)
 
 	if(reader->crdr.flush) { IO_Serial_Flush(reader); }
 
-	// define reader->gpio number used for card detect and reset. ref to globals.h
-	if(reader_use_gpio(reader))
-	{
-		reader->gpio_outen = open("/dev/gpio/outen", O_RDWR);
-		reader->gpio_out   = open("/dev/gpio/out",   O_RDWR);
-		reader->gpio_in    = open("/dev/gpio/in",    O_RDWR);
-		rdr_debug_mask(reader, D_IFD, "init gpio_outen:%d gpio_out:%d gpio_in:%d",
-					   reader->gpio_outen, reader->gpio_out, reader->gpio_in);
-		set_gpio_input(reader);
-	}
-
 	rdr_debug_mask(reader, D_IFD, "Initializing reader type=%d", reader->typ);
 
 	/* Default serial port settings */
@@ -322,18 +277,12 @@ int32_t Stinger_Reset(struct s_reader *reader, ATR *atr)
 
 		IO_Serial_Ioctl_Lock(reader, 1);
 
-		if(reader_use_gpio(reader))
-			{ set_gpio(reader, 0); }
-		else
-			{ IO_Serial_RTS_Set(reader); }
+		IO_Serial_RTS_Set(reader);
 
 		cs_sleepms(50);
 
-		// felix: set card reset hi (inactive)
-		if(reader_use_gpio(reader))
-			{ set_gpio_input(reader); }
-		else
-			{ IO_Serial_RTS_Clr(reader); }
+		IO_Serial_RTS_Clr(reader);
+		
 		cs_sleepms(50);
 
 		IO_Serial_Ioctl_Lock(reader, 0);
