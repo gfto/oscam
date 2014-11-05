449d5778f6a81908b932fe17eda4a8b002b4a49e
gorgone.impertinence <gorgone.impertinence@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2014-11-05 01:08:52 +0000

    add WebifPort: to oscam.version now its possible to read out for activity link in openwebif and link them to the main menu
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@9981 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam.c
+++ oscam.c
@@ -316,6 +316,7 @@ static void write_versionfile(bool use_stdout)
 	fprintf(fp, "Version:        oscam-%s-r%s\n", CS_VERSION, CS_SVN_VERSION);
 	fprintf(fp, "Compiler:       %s\n", CS_TARGET);
 	fprintf(fp, "ConfigDir:      %s\n", cs_confdir);
+	fprintf(fp, "WebifPort:      %d\n", cfg.http_port);
 	fprintf(fp, "\n");
 	write_conf(WEBIF, "Web interface support");
 	write_conf(WEBIF_LIVELOG, "LiveLog support");
