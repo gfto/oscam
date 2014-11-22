a8161b6de74b5a9f614942564169e3b592ea2c39
stefansat <stefansat@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2014-11-22 08:39:57 +0000

    Dumping rader or card bits.
    Added rdr_dump function. With this function reader based dumping is possible whitout the use of a debug mask.
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@10014 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam-log-reader.c
+++ oscam-log-reader.c
@@ -116,6 +116,16 @@ void rdr_debug_mask_sensitive(struct s_reader *reader, uint16_t mask, char *fmt,
 	}
 }
 
+void rdr_dump(struct s_reader *reader, const uint8_t *buf, int n, char *fmt, ...)
+{
+	char txt[2048];
+	va_list args;
+	va_start(args, fmt);
+	vsnprintf(txt, sizeof(txt), fmt, args);
+	va_end(args);
+	cs_dump(buf, n, "%s [%s] %s", reader->label, reader_desc_txt(reader), txt);
+}
+
 void rdr_ddump_mask(struct s_reader *reader, uint16_t mask, const uint8_t *buf, int n, char *fmt, ...)
 {
 	if(config_enabled(WITH_DEBUG))
--- oscam-log-reader.h
+++ oscam-log-reader.h
@@ -7,6 +7,7 @@ void rdr_log_sensitive(struct s_reader *reader, char *, ...) __attribute__((form
 void rdr_debug_mask(struct s_reader *reader, uint16_t mask, char *fmt, ...) __attribute__((format(printf, 3, 4)));
 void rdr_debug_mask_sensitive(struct s_reader *reader, uint16_t mask, char *fmt, ...) __attribute__((format(printf, 3, 4)));
 
+void rdr_dump(struct s_reader *reader, const uint8_t *buf, int n, char *fmt, ...) __attribute__((format(printf, 4, 5)));
 void rdr_ddump_mask(struct s_reader *reader, uint16_t mask, const uint8_t *buf, int n, char *fmt, ...) __attribute__((format(printf, 5, 6)));
 
 #endif
