9702b817acc9f4effaf3fba040013fd1f0cbd22d
gorgone.impertinence <gorgone.impertinence@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2014-12-22 18:05:18 +0000

    sort also with picons Thnx Hook
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@10095 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- webif/include/jscript.js
+++ webif/include/jscript.js
@@ -328,7 +328,7 @@ $(function () {
 		$("#dataTable tr").each(function (index) {
 			if (!index) return;
 			$(this).find("td").each(function () {
-				var id = $(this).text().toLowerCase().trim();
+				var id = (($(this).data('sort-value') == undefined || $(this).hasClass("usercol2")) ? $(this).text() : $(this).data('sort-value').toString()).toLowerCase().trim();
 				var not_found = (id.indexOf(value) == -1);
 				$(this).closest('tr').toggle(!not_found);
 				return not_found;
