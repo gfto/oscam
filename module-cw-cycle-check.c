9b741117dbb5f3e0e648a03fa641c206d41d9126
gf <gf@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2015-02-04 00:07:56 +0000

    cwcycle: Move private structures out of the header file.
    This structures are only used in module-cw-cycle-check. I'm leaving
    only the public stuff in the header file.
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@10392 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-cw-cycle-check.c
+++ module-cw-cycle-check.c
@@ -11,6 +11,35 @@
 #include "oscam-string.h"
 #include "oscam-cache.h"
 
+struct s_cwc_md5
+{
+	uchar           md5[CS_ECMSTORESIZE];
+	int32_t         csp_hash;
+	uchar           cw[16];
+};
+
+struct s_cw_cycle_check
+{
+	uchar           cw[16];
+	time_t          time;
+	time_t          locktime; // lock in learning
+	uint16_t        caid;
+	uint16_t        sid;
+	uint16_t        chid;
+	uint32_t        provid;
+	int16_t         ecmlen;
+	int8_t          stage;
+	int32_t         cycletime;
+	int32_t         dyncycletime;
+	int8_t          nextcyclecw;
+	struct s_cwc_md5    ecm_md5[15]; // max 15 old ecm md5 /csp-hashs
+	int8_t          cwc_hist_entry;
+	uint8_t         old;
+	int8_t			stage4_repeat;
+	struct s_cw_cycle_check *prev;
+	struct s_cw_cycle_check *next;
+};
+
 extern CS_MUTEX_LOCK cwcycle_lock;
 
 static struct s_cw_cycle_check *cw_cc_list;
--- module-cw-cycle-check.h
+++ module-cw-cycle-check.h
@@ -1,41 +1,12 @@
 #ifndef MODULE_CW_CYCLE_CHECK_H_
 #define MODULE_CW_CYCLE_CHECK_H_
 
-#ifdef CW_CYCLE_CHECK
-
-struct s_cwc_md5
-{
-	uchar           md5[CS_ECMSTORESIZE];
-	int32_t         csp_hash;
-	uchar           cw[16];
-};
-
-struct s_cw_cycle_check
-{
-	uchar           cw[16];
-	time_t          time;
-	time_t          locktime; // lock in learning
-	uint16_t        caid;
-	uint16_t        sid;
-	uint16_t        chid;
-	uint32_t        provid;
-	int16_t         ecmlen;
-	int8_t          stage;
-	int32_t         cycletime;
-	int32_t         dyncycletime;
-	int8_t          nextcyclecw;
-	struct s_cwc_md5    ecm_md5[15]; // max 15 old ecm md5 /csp-hashs
-	int8_t          cwc_hist_entry;
-	uint8_t         old;
-	int8_t			stage4_repeat;
-	struct s_cw_cycle_check *prev;
-	struct s_cw_cycle_check *next;
-};
-
-void cleanupcwcycle(void);
 uint8_t checkcwcycle(struct s_client *client, ECM_REQUEST *er, struct s_reader *reader, uchar *cw, int8_t rc, uint8_t cycletime_fr, uint8_t next_cw_cycle_fr);
+
+#ifdef CW_CYCLE_CHECK
+void cleanupcwcycle(void);
 #else
-static inline void cleanupcwcycle(void) { };
+static inline void cleanupcwcycle(void) { }
 #endif
 
 #endif
