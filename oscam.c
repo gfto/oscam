77d1181e5a52a781564e08733f063750827b5cb9
stefansat <stefansat@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2015-02-16 20:53:50 +0000

    Machine info
    Azbox premium
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@10550 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam.c
+++ oscam.c
@@ -739,8 +739,11 @@ static void init_machine_info(void)
 
 	read_line_from_file("/proc/stb/info/model", model, sizeof(model));
 	read_line_from_file("/proc/stb/info/boxtype", boxtype, sizeof(boxtype));
-	if (read_line_from_file("/proc/stb/info/vumodel", vumodel, sizeof(vumodel)) && !boxtype[0])
+	read_line_from_file("/proc/stb/info/vumodel", vumodel, sizeof(vumodel));
+	if (vumodel[0] && !boxtype[0] && strncasecmp(model,"premium",7))
 		snprintf(boxtype, sizeof(boxtype), "vu%s", vumodel);
+	if (!boxtype[0] && !strncasecmp(model,"premium",7) && !strcasecmp(vumodel, "ultimo"))
+		snprintf(boxtype, sizeof(boxtype), "%s", "Azbox-premium");
 
 	// Detect dreambox type
 	if (strcasecmp(buffer.machine, "ppc") == 0 && !model[0] && !boxtype[0])
