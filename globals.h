0ed2ee5cf39ddeb699f6f3a624e63738af5ecd5d
Aeon <Aeon@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2015-06-10 09:43:33 +0000

    small fix for stapi5 commit
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@10700 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- globals.h
+++ globals.h
@@ -909,7 +909,7 @@ typedef struct ecm_request_t
 #ifdef CW_CYCLE_CHECK
 	char            cwc_msg_log[MSGLOGSIZE];
 #endif
-#if defined(WITH_STAPI) || defined(WITH_STAPI5)
+#ifdef WITH_STAPI5
 	char			dev_name[20];
 #endif
 	struct ecm_request_t    *parent;
