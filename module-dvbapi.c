3b982f203d8d5aaa16f99622a0be1b1f99d6636b
Aeon <Aeon@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2015-06-26 16:17:54 +0000

    fix compile error
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@10832 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-dvbapi.c
+++ module-dvbapi.c
@@ -2978,11 +2978,9 @@ static uint32_t dvbapi_extract_sdt_string(char *buf, uint32_t buflen, uint8_t* s
 	uint32_t i, j, offset = 0;
 	int8_t iso_mode = -1;
 	char *tmpbuf;
-#ifdef READ_SDT_CHARSETS
 	const unsigned char *ptr_in;
 	unsigned char *ptr_out;
 	size_t in_bytes, out_bytes;
-#endif
 
 	if(sourcelen == 0)
 	{
