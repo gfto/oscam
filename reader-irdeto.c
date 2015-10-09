852acd965c112603f74d94e63d4879ac5227b879
theparasol <theparasol@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2015-10-09 19:12:36 +0000

    IRDETO:
    - More fix emm irdeto card status return (tnx newcslover!)
    
    
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@11049 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- reader-irdeto.c
+++ reader-irdeto.c
@@ -1055,11 +1055,15 @@ static int32_t irdeto_do_emm(struct s_reader *reader, EMM_PACKET *ep)
 				sc_Acs57_Cmd[4] = acslength;
 				reader_chk_cmd(sc_Acs57_Cmd, acslength + 2);
 				rdr_log_dbg(reader, D_EMM,"response %02X %02X %02X %02X %02X (%s)", cta_res[0], cta_res[1], cta_res[2], cta_res[3], cta_res[4], (cta_res[2] == 0 ? "OK" : "ERROR"));
-				if(cta_res[2] != 0)
-				{ 
-					return ERROR;
+				if(cta_res[2] == 0x7B || cta_res[2] == 0x7C) // chid already written or chid already up to date
+				{
+					return SKIPPED;
 				}
-				return OK;
+				if(cta_res[2] == 0x00)
+				{ 
+					return OK;
+				}
+				return ERROR; // all other 
 			}
 			else
 			{
@@ -1082,7 +1086,15 @@ static int32_t irdeto_do_emm(struct s_reader *reader, EMM_PACKET *ep)
 				int32_t errorcode = irdeto_do_cmd(reader, cta_cmd, 0, cta_res, &cta_lr);
 				rdr_log_dbg(reader, D_EMM,"response %02X %02X %02X %02X %02X (%s)", cta_res[0], cta_res[1], cta_res[2], cta_res[3], cta_res[4], (errorcode == 0 ? "OK" : "ERROR"));
 				
-				return (errorcode == 0 ? OK : ERROR);
+				if(cta_res[2] == 0x7B || cta_res[2] == 0x7C) // chid already written or chid already up to date
+				{
+					return SKIPPED;
+				}
+				if(cta_res[2] == 0x00)
+				{ 
+					return OK;
+				}
+				return ERROR; // all other
 			}
 		}
 		else
