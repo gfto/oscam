b3530c9e5713e59fe6e6a13c5187afacc1d28e18
gorgone.impertinence <gorgone.impertinence@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2015-11-29 17:09:18 +0000

    small cosmetic fix to hide inactive clients
    git-svn-id: http://streamboard.de.vu/svn/oscam/trunk@11189 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam-config-global.c
+++ oscam-config-global.c
@@ -517,7 +517,7 @@ static const struct config_list webif_opts[] =
 	DEF_OPT_INT8("http_prepend_embedded_css"   , OFS(http_prepend_embedded_css), 0),
 	DEF_OPT_INT32("httprefresh"             , OFS(http_refresh),            0),
 	DEF_OPT_INT32("httppollrefresh"         , OFS(poll_refresh),            60),
-	DEF_OPT_INT8("httphideidleclients"      , OFS(http_hide_idle_clients),  0),
+	DEF_OPT_INT8("httphideidleclients"      , OFS(http_hide_idle_clients),  1),
 	DEF_OPT_STR("httphidetype"              , OFS(http_hide_type),          NULL),
 	DEF_OPT_INT8("httpshowpicons"           , OFS(http_showpicons),         0),
 	DEF_OPT_INT8("httppiconsize"            , OFS(http_picon_size),         0),
